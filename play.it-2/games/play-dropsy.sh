5e2f709cd17577caabde3e7848380421e61758b7
vv221 <vv221@dotslashplay.it>
2018-07-25 12:12:45 +0200

    2.9 - Dropsy - Ensure that PulseAudio is running before starting the game, to avoid a crash

--- play.it-2/games/play-dropsy.sh
+++ play.it-2/games/play-dropsy.sh
@@ -35,7 +35,7 @@ set -o errexit
 # send your bug reports to mopi@dotslashplay.it
 ###
 
-script_version=20180725.1
+script_version=20180725.2
 
 # Set game-specific variables
 
@@ -63,6 +63,7 @@ ARCHIVE_GAME_DATA_FILES='./*_Data'
 DATA_DIRS='./logs'
 
 APP_MAIN_TYPE='native'
+APP_MAIN_PRERUN='pulseaudio --start'
 APP_MAIN_EXE_BIN32='Dropsy.x86'
 APP_MAIN_EXE_BIN64='Dropsy.x86_64'
 APP_MAIN_OPTIONS='-logFile ./logs/$(date +%F-%R).log'
@@ -75,7 +76,7 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN32_ARCH='32'
-PKG_BIN32_DEPS="$PKG_DATA_ID glibc glx xcursor libxrandr"
+PKG_BIN32_DEPS="$PKG_DATA_ID glibc glx xcursor libxrandr pulseaudio"
 
 PKG_BIN64_ARCH='64'
 PKG_BIN64_DEPS="$PKG_BIN32_DEPS_DEB"
