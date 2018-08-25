7097fd4940c5dd79789a13619d4abd586e2c1c3f
Dominique DERRIER <derrierdo@gmail.com>
2018-08-25 10:14:54 -0400

    Little Big Adventure | GOG

---
+++ play.it-2/games/play-little-big-adventure-1.sh
@@ -0,0 +1,133 @@
+#!/bin/sh -e
+set -o errexit
+
+###
+# Copyright (c) 2015-2018, Antoine Le Gonidec
+# Copyright (c) 2018, Dominique Derrier
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# This software is provided by the copyright holders and contributors "as is"
+# and any express or implied warranties, including, but not limited to, the
+# implied warranties of merchantability and fitness for a particular purpose
+# are disclaimed. In no event shall the copyright holder or contributors be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of the
+# possibility of such damage.
+###
+
+###
+# Little Big Adventure 1
+# build native Linux packages from the original installers
+# send your bug reports to vv221@dotslashplay.it
+###
+
+script_version=20180828.1
+
+# Set game-specific variables
+
+GAME_ID='little-big-adventure-1'
+GAME_NAME='Little Big Adventure'
+
+ARCHIVE_GOG='setup_lba_2.1.0.22.exe'
+ARCHIVE_GOG_URL='https://www.gog.com/game/little_big_adventure'
+ARCHIVE_GOG_MD5='c40177522adcbe50ea52590be57045f8'
+ARCHIVE_GOG_SIZE='510000'
+ARCHIVE_GOG_VERSION='1.0-gog2.1.0.22'
+
+ARCHIVE_DOC_DATA_PATH='app'
+ARCHIVE_DOC_DATA_FILES='*.pdf *.txt'
+
+ARCHIVE_GAME_BIN_PATH='app'
+ARCHIVE_GAME_BIN_FILES='*.cfg *.dll *.ini dos4gw.exe language.exe relent.exe loadpats.exe setup.exe setup.lst'
+
+ARCHIVE_GAME_DATA_PATH='app'
+ARCHIVE_GAME_DATA_FILES='*.hqr lba.dat lba.gog setsound.bat vox sample.*'
+
+GAME_IMAGE='lba.dat'
+
+CONFIG_FILES='./*.cfg ./*.ini'
+DATA_FILES='./*.LBA'
+
+APP_MAIN_TYPE='dosbox'
+APP_MAIN_EXE='relent.exe'
+APP_MAIN_ICON='app/goggame-1207658971.ico'
+
+PACKAGES_LIST='PKG_BIN PKG_DATA'
+
+PKG_DATA_ID="${GAME_ID}-data"
+PKG_DATA_DESCRIPTION='data'
+
+PKG_BIN_ARCH='32'
+PKG_BIN_DEPS="$PKG_DATA_ID dosbox"
+
+# Load common functions
+
+target_version='2.10'
+if [ -z "$PLAYIT_LIB2" ]; then
+        [ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
+        for path in\
+                './'\
+                "$XDG_DATA_HOME/play.it/"\
+                "$XDG_DATA_HOME/play.it/play.it-2/lib/"\
+                '/usr/local/share/games/play.it/'\
+                '/usr/local/share/play.it/'\
+                '/usr/share/games/play.it/'\
+                '/usr/share/play.it/'
+        do
+                if [ -z "$PLAYIT_LIB2" ] && [ -e "$path/libplayit2.sh" ]; then
+                        PLAYIT_LIB2="$path/libplayit2.sh"
+                        break
+                fi
+        done
+        if [ -z "$PLAYIT_LIB2" ]; then
+                printf '\n\033[1;31mError:\033[0m\n'
+                printf 'libplayit2.sh not found.\n'
+                exit 1
+        fi
+fi
+. "$PLAYIT_LIB2"
+
+# Extract game data
+
+extract_data_from "$SOURCE_ARCHIVE"
+prepare_package_layout
+
+# Extract icons
+
+PKG='PKG_DATA'
+icons_get_from_workdir 'APP_MAIN'
+rm --recursive "$PLAYIT_WORKDIR/gamedata"
+
+# Write launchers
+
+PKG='PKG_BIN'
+write_launcher 'APP_MAIN'
+
+# Build package
+
+write_metadata
+build_pkg
+
+# Clean up
+
+rm --recursive "$PLAYIT_WORKDIR"
+
+# Print instructions
+
+print_instructions
+
+exit 0
