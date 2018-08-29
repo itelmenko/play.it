512fe931feb508185314d407d23fe0951b8f30b4
BetaRays <BetaRays+framagit@protonmail.com>
2018-08-29 20:08:12 +0200

    LIMBO - New 2.10 script - Initially supported by ./play.it 1.14

---
+++ play.it-2/games/play-limbo.sh
@@ -0,0 +1,130 @@
+#!/bin/sh -e
+set -o errexit
+
+###
+# Copyright (c) 2015-2018, Antoine Le Gonidec
+# Copyright (c) 2018, BetaRays
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# This software is provided by the copyright holders and contributors "as is"
+# and any express or implied warranties, including, but not limited to, the
+# implied warranties of merchantability and fitness for a particular purpose
+# are disclaimed. In no event shall the copyright holder or contributors be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of the
+# possibility of such damage.
+###
+
+###
+# LIMBO
+# build native Linux packages from the original installers
+# send your bug reports to vv221@dotslashplay.it
+###
+
+script_version=20180910.1
+
+# Set game-specific variables
+
+GAME_ID='limbo'
+GAME_NAME='LIMBO'
+
+ARCHIVE_HUMBLE='Limbo-Linux-2014-06-18.sh'
+ARCHIVE_HUMBLE_URL='https://www.humblebundle.com/store/limbo'
+ARCHIVE_HUMBLE_MD5='9b453abcb859c31cc645a7207de08329'
+ARCHIVE_HUMBLE_VERSION='1.3-humble140618'
+ARCHIVE_HUMBLE_SIZE='120000'
+ARCHIVE_HUMBLE_TYPE='mojosetup'
+
+ARCHIVE_DOC_DATA_PATH='data'
+ARCHIVE_DOC_DATA_PATH='./README-linux.txt'
+
+ARCHIVE_GAME_BIN_PATH='data'
+ARCHIVE_GAME_BIN_FILES='./limbo'
+
+ARCHIVE_GAME_DATA_PATH='data'
+ARCHIVE_GAME_DATA_FILES='./settings.txt ./limbo.png ./data'
+
+CONFIG_FILES='./settings.txt'
+
+APP_MAIN_TYPE='native'
+APP_MAIN_EXE_BIN='limbo'
+APP_MAIN_ICON='limbo.png'
+
+PACKAGES_LIST='PKG_BIN PKG_DATA'
+
+PKG_DATA_ID="${GAME_ID}-data"
+PKG_DATA_DESCRIPTION='data'
+
+PKG_BIN_ARCH='32'
+PKG_BIN_DEPS="$PKG_DATA_ID sdl2"
+
+# Load common functions
+
+target_version='2.10'
+
+if [ -z "$PLAYIT_LIB2" ]; then
+	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
+	for path in\
+		'./'\
+		"$XDG_DATA_HOME/play.it/"\
+		"$XDG_DATA_HOME/play.it/play.it-2/lib/"\
+		'/usr/local/share/games/play.it/'\
+		'/usr/local/share/play.it/'\
+		'/usr/share/games/play.it/'\
+		'/usr/share/play.it/'
+	do
+		if [ -z "$PLAYIT_LIB2" ] && [ -e "$path/libplayit2.sh" ]; then
+			PLAYIT_LIB2="$path/libplayit2.sh"
+			break
+		fi
+	done
+	if [ -z "$PLAYIT_LIB2" ]; then
+		printf '\n\033[1;31mError:\033[0m\n'
+		printf 'libplayit2.sh not found.\n'
+		exit 1
+	fi
+fi
+. "$PLAYIT_LIB2"
+
+# Extract game data
+
+extract_data_from "$SOURCE_ARCHIVE"
+prepare_package_layout
+rm --recursive "$PLAYIT_WORKDIR/gamedata"
+
+# Write launchers
+
+PKG='PKG_BIN'
+write_launcher 'APP_MAIN'
+
+# Build package
+
+PKG='PKG_DATA'
+icons_linking_postinst 'APP_MAIN'
+write_metadata 'PKG_DATA'
+write_metadata 'PKG_BIN'
+build_pkg
+
+# Clean up
+
+rm --recursive "$PLAYIT_WORKDIR"
+
+# Print instructions
+
+print_instructions
+
+exit 0
