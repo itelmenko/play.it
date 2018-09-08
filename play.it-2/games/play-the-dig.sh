643b101a0a07fbf1118dd59f4b19a5a94928ba46
vv221 <vv221@dotslashplay.it>
2018-09-08 22:00:32 +0200

    The Dig - New script - Contributed by Elzen

---
+++ play.it-2/games/play-the-dig.sh
@@ -0,0 +1,134 @@
+#!/bin/sh -e
+set -o errexit
+
+###
+# Copyright (c) 2015-2018, Antoine Le Gonidec,
+# Copyright (c) 2018 Sébastien “Elzen” Dufromentel
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# This software is provided by the copyright holders and contributors "as is"
+# and any express or implied warranties, including, but not limited to, the
+# implied warranties of merchantability and fitness for a particular purpose
+# are disclaimed. In no event shall the copyright holder or contributors be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of the
+# possibility of such damage.
+###
+
+###
+# The Dig
+# build native packages from the original installers
+# send your bug reports to vv221@dotslashplay.it
+###
+
+script_version=20180918.1
+
+# Set game-specific variables
+
+GAME_ID='the-dig'
+GAME_NAME='The Dig'
+
+ARCHIVES_LIST='ARCHIVE_GOG_EN ARCHIVE_GOG_FR'
+
+ARCHIVE_GOG_EN='the_dig_en_gog_2_20100.sh'
+ARCHIVE_GOG_EN_URL='https://www.gog.com/game/the_dig'
+ARCHIVE_GOG_EN_MD5='0fd830de17757f78dc9865dd7c06c785'
+ARCHIVE_GOG_EN_SIZE='760000'
+ARCHIVE_GOG_EN_VERSION='1.0-gog20100'
+ARCHIVE_GOG_EN_TYPE='mojosetup'
+
+ARCHIVE_GOG_FR='the_dig_fr_gog_2_20100.sh'
+ARCHIVE_GOG_FR_URL='https://www.gog.com/game/the_dig'
+ARCHIVE_GOG_FR_MD5='b4c2b87f0305a82bb0fa805b01b014f1'
+ARCHIVE_GOG_FR_SIZE='760000'
+ARCHIVE_GOG_FR_VERSION='1.0-gog20100'
+ARCHIVE_GOG_FR_TYPE='mojosetup'
+
+ARCHIVE_DOC_MAIN_PATH='data/noarch/docs'
+ARCHIVE_DOC_MAIN_FILES='*'
+
+ARCHIVE_GAME_MAIN_PATH='data/noarch/data'
+ARCHIVE_GAME_MAIN_FILES='dig.la0 dig.la1 digmusic.bun digvoice.bun language.bnd video'
+
+APP_MAIN_TYPE='scummvm'
+APP_MAIN_SCUMMID='dig'
+APP_MAIN_ICON='data/noarch/support/icon.png'
+
+PACKAGES_LIST='PKG_MAIN'
+
+PKG_MAIN_ID="$GAME_ID"
+PKG_MAIN_ID_GOG_EN="${GAME_ID}-en"
+PKG_MAIN_ID_GOG_FR="${GAME_ID}-fr"
+PKG_MAIN_PROVIDE="$PKG_MAIN_ID"
+PKG_MAIN_DEPS='scummvm'
+
+# Load common functions
+
+target_version='2.10'
+
+if [ -z "$PLAYIT_LIB2" ]; then
+	: ${XDG_DATA_HOME:="$HOME/.local/share"}
+	for path in\
+		"$PWD"\
+		"$XDG_DATA_HOME/play.it"\
+		'/usr/local/share/games/play.it'\
+		'/usr/local/share/play.it'\
+		'/usr/share/games/play.it'\
+		'/usr/share/play.it'
+	do
+		if [ -e "$path/libplayit2.sh" ]; then
+			PLAYIT_LIB2="$path/libplayit2.sh"
+			break
+		fi
+	done
+fi
+if [ -z "$PLAYIT_LIB2" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'libplayit2.sh not found.\n'
+	exit 1
+fi
+. "$PLAYIT_LIB2"
+
+# Extract data from game
+
+extract_data_from "$SOURCE_ARCHIVE"
+tolower "$PLAYIT_WORKDIR/gamedata"
+prepare_package_layout
+
+# Get icon
+
+icons_get_from_workdir 'APP_MAIN'
+rm --recursive "$PLAYIT_WORKDIR/gamedata"
+
+# Write launchers
+
+write_launcher 'APP_MAIN'
+
+# Build package
+
+write_metadata
+build_pkg
+
+# Clean up
+
+rm --recursive "$PLAYIT_WORKDIR"
+
+# Print instructions
+
+print_instructions
+
+exit 0
