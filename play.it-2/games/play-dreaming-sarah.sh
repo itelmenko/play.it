2617c34172c9e7a5c36375893fbffa37349fcad0
vv221 <vv221@dotslashplay.it>
2018-09-13 17:48:43 +0200

    Dreaming Sarah — Update script to 2.10 syntax + Update author list + Update contact e-mail

--- play.it-2/games/play-dreaming-sarah.sh
+++ play.it-2/games/play-dreaming-sarah.sh
@@ -3,6 +3,7 @@ set -o errexit
 
 ###
 # Copyright (c) 2015-2018, Antoine Le Gonidec
+# Copyright (c) 2018, Solène Huault
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -30,11 +31,11 @@ set -o errexit
 
 ###
 # Dreaming Sarah
-# build native Linux packages from the original installers
-# send your bug reports to vv221@dotslashplay.it
+# build native packages from the original installers
+# send your bug reports to mopi@dotslashplay.it
 ###
 
-script_version=20180310.2
+script_version=20180913.1
 
 # Set game-specific variables
 
@@ -57,11 +58,11 @@ ARCHIVE_HUMBLE_64_VERSION='1.3-humble1'
 
 ARCHIVE_GAME_BIN_PATH_HUMBLE_32='DreamingSarah-linux32'
 ARCHIVE_GAME_BIN_PATH_HUMBLE_64='DreamingSarah-linux64'
-ARCHIVE_GAME_BIN_FILES='./lib ./nacl_helper ./nacl_helper_bootstrap ./nw'
+ARCHIVE_GAME_BIN_FILES='lib nacl_helper* nw'
 
 ARCHIVE_GAME_DATA_PATH_HUMBLE_32='DreamingSarah-linux32'
 ARCHIVE_GAME_DATA_PATH_HUMBLE_64='DreamingSarah-linux64'
-ARCHIVE_GAME_DATA_FILES='./icudtl.dat ./locales ./natives_blob.bin ./nw_100_percent.pak ./nw_200_percent.pak ./package.nw ./resources.pak'
+ARCHIVE_GAME_DATA_FILES='*.bin *.dat *.nw *.pak locales'
 
 APP_MAIN_TYPE='native'
 APP_MAIN_EXE='nw'
@@ -78,19 +79,28 @@ PKG_BIN_DEPS="$PKG_DATA_ID glibc libstdc++ libxrandr xcursor nss gconf gtk2"
 
 # Load common functions
 
-target_version='2.6'
+target_version='2.10'
 
 if [ -z "$PLAYIT_LIB2" ]; then
-	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
-	if [ -e "$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh" ]; then
-		PLAYIT_LIB2="$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh"
-	elif [ -e './libplayit2.sh' ]; then
-		PLAYIT_LIB2='./libplayit2.sh'
-	else
-		printf '\n\033[1;31mError:\033[0m\n'
-		printf 'libplayit2.sh not found.\n'
-		exit 1
-	fi
+	: ${XDG_DATA_HOME:="$HOME/.local/share"}
+	for path in\
+		"$PWD"\
+		"$XDG_DATA_HOME/play.it"\
+		'/usr/local/share/games/play.it'\
+		'/usr/local/share/play.it'\
+		'/usr/share/games/play.it'\
+		'/usr/share/play.it'
+	do
+		if [ -e "$path/libplayit2.sh" ]; then
+			PLAYIT_LIB2="$path/libplayit2.sh"
+			break
+		fi
+	done
+fi
+if [ -z "$PLAYIT_LIB2" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'libplayit2.sh not found.\n'
+	exit 1
 fi
 . "$PLAYIT_LIB2"
 
