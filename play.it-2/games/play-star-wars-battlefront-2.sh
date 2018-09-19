dd85a29b86e230f8c081a6a6c728b8f79f183872
vv221 <vv221@dotslashplay.it>
2018-09-19 17:11:37 +0200

    Star Wars Battlefront II — New archive

--- play.it-2/games/play-star-wars-battlefront-2.sh
+++ play.it-2/games/play-star-wars-battlefront-2.sh
@@ -34,44 +34,65 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20180919.1
+script_version=20180919.2
 
 # Set game-specific variables
 
 GAME_ID='star-wars-battlefront-2'
 GAME_NAME='Star Wars Battlefront II'
 
-ARCHIVE_GOG='setup_sw_battlefront2_2.0.0.5.exe'
+ARCHIVE_GOG='setup_star_wars_battlefront_ii_1.1_multiplayer_update_2_(17606).exe'
 ARCHIVE_GOG_URL='https://www.gog.com/game/star_wars_battlefront_ii'
-ARCHIVE_GOG_MD5='51284c8a8e777868219e811ada284fb1'
-ARCHIVE_GOG_VERSION='1.1-gog2.0.0.5'
-ARCHIVE_GOG_SIZE='9100000'
-ARCHIVE_GOG_TYPE='rar'
-ARCHIVE_GOG_GOGID='1421404701'
-ARCHIVE_GOG_PART1='setup_sw_battlefront2_2.0.0.5-1.bin'
-ARCHIVE_GOG_PART1_MD5='dc36b03c9c43fb8d3cb9b92c947daaa4'
-ARCHIVE_GOG_PART1_TYPE='rar'
-ARCHIVE_GOG_PART2='setup_sw_battlefront2_2.0.0.5-2.bin'
-ARCHIVE_GOG_PART2_MD5='5d4000fd480a80b6e7c7b73c5a745368'
-ARCHIVE_GOG_PART2_TYPE='rar'
+ARCHIVE_GOG_MD5='f482ec251067336d3b8211774b4c44f6'
+ARCHIVE_GOG_VERSION='1.1.2-gog17606'
+ARCHIVE_GOG_SIZE='11000000'
+ARCHIVE_GOG_TYPE='innosetup1.7'
+ARCHIVE_GOG_PART1='setup_star_wars_battlefront_ii_1.1_multiplayer_update_2_(17606)-1.bin'
+ARCHIVE_GOG_PART1_MD5='c34b41f594e55b1522d8826f19cf958f'
+ARCHIVE_GOG_PART1_TYPE='innosetup1.7'
+ARCHIVE_GOG_PART2='setup_star_wars_battlefront_ii_1.1_multiplayer_update_2_(17606)-2.bin'
+ARCHIVE_GOG_PART2_MD5='c9423f3983c67575c1c531e0d18e6a0f'
+ARCHIVE_GOG_PART2_TYPE='innosetup1.7'
 
-ARCHIVE_DOC_DATA_PATH='game'
+ARCHIVE_GOG_OLD0='setup_sw_battlefront2_2.0.0.5.exe'
+ARCHIVE_GOG_OLD0_MD5='51284c8a8e777868219e811ada284fb1'
+ARCHIVE_GOG_OLD0_VERSION='1.1-gog2.0.0.5'
+ARCHIVE_GOG_OLD0_SIZE='9100000'
+ARCHIVE_GOG_OLD0_TYPE='rar'
+ARCHIVE_GOG_OLD0_GOGID='1421404701'
+ARCHIVE_GOG_OLD0_PART1='setup_sw_battlefront2_2.0.0.5-1.bin'
+ARCHIVE_GOG_OLD0_PART1_MD5='dc36b03c9c43fb8d3cb9b92c947daaa4'
+ARCHIVE_GOG_OLD0_PART1_TYPE='rar'
+ARCHIVE_GOG_OLD0_PART2='setup_sw_battlefront2_2.0.0.5-2.bin'
+ARCHIVE_GOG_OLD0_PART2_MD5='5d4000fd480a80b6e7c7b73c5a745368'
+ARCHIVE_GOG_OLD0_PART2_TYPE='rar'
+
+ARCHIVE_DOC_DATA_PATH='.'
 ARCHIVE_DOC_DATA_FILES='*.pdf'
+# Keep compatibility with old archives
+ARCHIVE_DOC_DATA_PATH_GOG_OLD0='game'
 
-ARCHIVE_GAME_BIN_PATH='game/gamedata'
+ARCHIVE_GAME_BIN_PATH='gamedata'
 ARCHIVE_GAME_BIN_FILES='*.exe binkw32.dll eax.dll unicows.dll'
+# Keep compatibility with old archives
+ARCHIVE_GAME_BIN_PATH_GOG_OLD0='game/gamedata'
 
-ARCHIVE_GAME_MOVIES_PATH='game/gamedata'
+ARCHIVE_GAME_MOVIES_PATH='gamedata'
 ARCHIVE_GAME_MOVIES_FILES='data/_lvl_pc/movies'
+# Keep compatibility with old archives
+ARCHIVE_GAME_MOVIES_PATH_GOG_OLD0='game/gamedata'
 
-ARCHIVE_GAME_DATA_PATH='game/gamedata'
+ARCHIVE_GAME_DATA_PATH='gamedata'
 ARCHIVE_GAME_DATA_FILES='data'
+# Keep compatibility with old archives
+ARCHIVE_GAME_DATA_PATH_GOG_OLD0='game/gamedata'
 
 DATA_DIRS='./savegames'
 
 APP_MAIN_TYPE='wine'
 APP_MAIN_EXE='battlefrontii.exe'
-APP_MAIN_ICON='battlefrontii.exe'
+# Keep compatibility with old archives
+APP_MAIN_ICON_GOG_OLD0='battlefrontii.exe'
 
 PACKAGES_LIST='PKG_MOVIES PKG_BIN PKG_DATA'
 
@@ -115,18 +136,28 @@ fi
 
 # Extract game data
 
-ln --symbolic "$(readlink --canonicalize "$ARCHIVE_PART1")" "$PLAYIT_WORKDIR/$GAME_ID.r00"
-ln --symbolic "$(readlink --canonicalize "$ARCHIVE_PART2")" "$PLAYIT_WORKDIR/$GAME_ID.r01"
-extract_data_from "$PLAYIT_WORKDIR/$GAME_ID.r00"
-tolower "$PLAYIT_WORKDIR/gamedata"
+case "$ARCHIVE" in
+	('ARCHIVE_GOG_OLD0')
+		ln --symbolic "$(readlink --canonicalize "$SOURCE_ARCHIVE_PART1")" "$PLAYIT_WORKDIR/$GAME_ID.r00"
+		ln --symbolic "$(readlink --canonicalize "$SOURCE_ARCHIVE_PART2")" "$PLAYIT_WORKDIR/$GAME_ID.r01"
+		extract_data_from "$PLAYIT_WORKDIR/$GAME_ID.r00"
+		tolower "$PLAYIT_WORKDIR/gamedata"
+	;;
+	(*)
+		extract_data_from "$SOURCE_ARCHIVE"
+	;;
+esac
 prepare_package_layout
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Estract icons
 
-PKG='PKG_BIN'
-icons_get_from_package 'APP_MAIN'
-icons_move_to 'PKG_DATA'
+use_archive_specific_value 'APP_MAIN_ICON'
+if [ "$APP_MAIN_ICON" ]; then
+	PKG='PKG_BIN'
+	icons_get_from_package 'APP_MAIN'
+	icons_move_to 'PKG_DATA'
+fi
 
 # Write launchers
 
