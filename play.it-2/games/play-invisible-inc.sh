cfc0bd803a6559f34e68857d5fadc7e05811ba6d
vv221 <vv221@dotslashplay.it>
2018-09-19 12:11:11 +0200

    Insivisible Inc. — Update script to 2.10 syntax

--- play.it-2/games/play-invisible-inc.sh
+++ play.it-2/games/play-invisible-inc.sh
@@ -30,18 +30,16 @@ set -o errexit
 
 ###
 # Invisible Inc.
-# build native Linux packages from the original installers
+# build native packages from the original installers
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20180224.1
+script_version=20180919.1
 
 # Set game-specific variables
 
 GAME_ID='invisible-inc'
 GAME_NAME='Invisible Inc.'
-
-ARCHIVES_LIST='ARCHIVE_GOG ARCHIVE_GOG_OLD'
 
 ARCHIVE_GOG='invisible_inc_en_8_07_2017_15873.sh'
 ARCHIVE_GOG_URL='https://www.gog.com/game/invisible_inc'
@@ -50,31 +48,31 @@ ARCHIVE_GOG_SIZE='1200000'
 ARCHIVE_GOG_VERSION='2017.07.08-gog15873'
 ARCHIVE_GOG_TYPE='mojosetup_unzip'
 
-ARCHIVE_GOG_OLD='gog_invisible_inc_2.6.0.11.sh'
-ARCHIVE_GOG_OLD_MD5='97e6efdc9237ec17deb02b5cf5185cf5'
-ARCHIVE_GOG_OLD_SIZE='1200000'
-ARCHIVE_GOG_OLD_VERSION='2016.04.13-gog2.6.0.11'
+ARCHIVE_GOG_OLD0='gog_invisible_inc_2.6.0.11.sh'
+ARCHIVE_GOG_OLD0_MD5='97e6efdc9237ec17deb02b5cf5185cf5'
+ARCHIVE_GOG_OLD0_SIZE='1200000'
+ARCHIVE_GOG_OLD0_VERSION='2016.04.13-gog2.6.0.11'
 
 ARCHIVE_ICONS_PACK='invisible-inc_icons.tar.gz'
 ARCHIVE_ICONS_PACK_MD5='37a62fed1dc4185e95db3e82e6695c1d'
 
-ARCHIVE_DOC1_DATA_PATH='data/noarch/docs'
-ARCHIVE_DOC1_DATA_FILES='./*'
+ARCHIVE_DOC0_DATA_PATH='data/noarch/docs'
+ARCHIVE_DOC0_DATA_FILES='*'
 
-ARCHIVE_DOC2_DATA_PATH='data/noarch/game'
-ARCHIVE_DOC2_DATA_FILES='./LICENSE'
+ARCHIVE_DOC1_DATA_PATH='data/noarch/game'
+ARCHIVE_DOC1_DATA_FILES='LICENSE'
 
 ARCHIVE_GAME_BIN32_PATH='data/noarch/game'
-ARCHIVE_GAME_BIN32_FILES='./InvisibleInc32 ./lib32'
+ARCHIVE_GAME_BIN32_FILES='InvisibleInc32 lib32'
 
 ARCHIVE_GAME_BIN64_PATH='data/noarch/game'
-ARCHIVE_GAME_BIN64_FILES='./InvisibleInc64 ./lib64'
+ARCHIVE_GAME_BIN64_FILES='InvisibleInc64 lib64'
 
 ARCHIVE_GAME_DATA_PATH='data/noarch/game'
-ARCHIVE_GAME_DATA_FILES='./*.kwad ./*.lua ./hashes.dat ./scripts.zip'
+ARCHIVE_GAME_DATA_FILES='*.kwad *.lua hashes.dat scripts.zip'
 
 ARCHIVE_ICONS_PATH='.'
-ARCHIVE_ICONS_FILES='./16x16 ./32x32 ./64x64 ./128x128 ./256x256'
+ARCHIVE_ICONS_FILES='16x16 32x32 64x64 128x128 256x256'
 
 APP_MAIN_TYPE='native'
 APP_MAIN_LIBS_BIN32='lib32'
@@ -82,7 +80,6 @@ APP_MAIN_LIBS_BIN64='lib64'
 APP_MAIN_EXE_BIN32='InvisibleInc32'
 APP_MAIN_EXE_BIN64='InvisibleInc64'
 APP_MAIN_ICON_GOG='data/noarch/support/icon.png'
-APP_MAIN_ICON_GOG_RES='256'
 
 PACKAGES_LIST='PKG_DATA PKG_BIN32 PKG_BIN64'
 
@@ -97,19 +94,28 @@ PKG_BIN64_DEPS="$PKG_BIN32_DEPS"
 
 # Load common functions
 
-target_version='2.5'
+target_version='2.10'
 
 if [ -z "$PLAYIT_LIB2" ]; then
-	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
-	if [ -e "$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh" ]; then
-		PLAYIT_LIB2="$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh"
-	elif [ -e './libplayit2.sh' ]; then
-		PLAYIT_LIB2='./libplayit2.sh'
-	else
-		printf '\n\033[1;31mError:\033[0m\n'
-		printf 'libplayit2.sh not found.\n'
-		exit 1
-	fi
+	: ${XDG_DATA_HOME:="$HOME/.local/share"}
+	for path in\
+		"$PWD"\
+		"$XDG_DATA_HOME/play.it"\
+		'/usr/local/share/games/play.it'\
+		'/usr/local/share/play.it'\
+		'/usr/share/games/play.it'\
+		'/usr/share/play.it'
+	do
+		if [ -e "$path/libplayit2.sh" ]; then
+			PLAYIT_LIB2="$path/libplayit2.sh"
+			break
+		fi
+	done
+fi
+if [ -z "$PLAYIT_LIB2" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'libplayit2.sh not found.\n'
+	exit 1
 fi
 . "$PLAYIT_LIB2"
 
@@ -122,26 +128,21 @@ ARCHIVE="$ARCHIVE_MAIN"
 # Extract game data
 
 extract_data_from "$SOURCE_ARCHIVE"
+prepare_package_layout
+
+# Get icons
+
+PKG='PKG_DATA'
 if [ "$ARCHIVE_ICONS" ]; then  
 	(
+		rm --recursive "$PLAYIT_WORKDIR/gamedata"
 		ARCHIVE='ARCHIVE_ICONS'
 		extract_data_from "$ARCHIVE_ICONS"
 	)
-fi
-
-for PKG in $PACKAGES_LIST; do
-	organize_data "DOC1_${PKG#PKG_}" "$PATH_DOC"
-	organize_data "DOC2_${PKG#PKG_}" "$PATH_DOC"
-	organize_data "GAME_${PKG#PKG_}" "$PATH_GAME"
-done
-
-PKG='PKG_DATA'
-if [ "$ARCHIVE_ICONS" ]; then
 	organize_data 'ICONS' "$PATH_ICON_BASE"
 else
-	get_icon_from_temp_dir 'APP_MAIN'
+	icons_get_from_workdir 'APP_MAIN'
 fi
-
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Write launchers
@@ -161,10 +162,6 @@ rm --recursive "$PLAYIT_WORKDIR"
 
 # Print instructions
 
-printf '\n'
-printf '32-bit:'
-print_instructions 'PKG_DATA' 'PKG_BIN32'
-printf '64-bit:'
-print_instructions 'PKG_DATA' 'PKG_BIN64'
+print_instructions
 
 exit 0
