71d18a5239c6eae4d56c10ba6a217ae8bfaaee70
vv221 <vv221@dotslashplay.it>
2018-09-19 16:56:49 +0200

    Star Wars Battlefront II — Update script to 2.10 syntax

--- play.it-2/games/play-star-wars-battlefront-2.sh
+++ play.it-2/games/play-star-wars-battlefront-2.sh
@@ -30,18 +30,16 @@ set -o errexit
 
 ###
 # Star Wars Battlefront II
-# build native Linux packages from the original installers
+# build native packages from the original installers
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20180224.1
+script_version=20180919.1
 
 # Set game-specific variables
 
 GAME_ID='star-wars-battlefront-2'
 GAME_NAME='Star Wars Battlefront II'
-
-ARCHIVES_LIST='ARCHIVE_GOG'
 
 ARCHIVE_GOG='setup_sw_battlefront2_2.0.0.5.exe'
 ARCHIVE_GOG_URL='https://www.gog.com/game/star_wars_battlefront_ii'
@@ -58,25 +56,24 @@ ARCHIVE_GOG_PART2_MD5='5d4000fd480a80b6e7c7b73c5a745368'
 ARCHIVE_GOG_PART2_TYPE='rar'
 
 ARCHIVE_DOC_DATA_PATH='game'
-ARCHIVE_DOC_DATA_FILES='./*.pdf'
+ARCHIVE_DOC_DATA_FILES='*.pdf'
 
 ARCHIVE_GAME_BIN_PATH='game/gamedata'
-ARCHIVE_GAME_BIN_FILES='./*.exe ./binkw32.dll ./eax.dll ./unicows.dll'
+ARCHIVE_GAME_BIN_FILES='*.exe binkw32.dll eax.dll unicows.dll'
 
 ARCHIVE_GAME_MOVIES_PATH='game/gamedata'
-ARCHIVE_GAME_MOVIES_FILES='./data/_lvl_pc/movies'
+ARCHIVE_GAME_MOVIES_FILES='data/_lvl_pc/movies'
 
 ARCHIVE_GAME_DATA_PATH='game/gamedata'
-ARCHIVE_GAME_DATA_FILES='./data/_lvl_pc/*.def ./data/_lvl_pc/*.lvl ./data/_lvl_pc/bes ./data/_lvl_pc/common ./data/_lvl_pc/cor ./data/_lvl_pc/dag ./data/_lvl_pc/dea ./data/_lvl_pc/end ./data/_lvl_pc/fel ./data/_lvl_pc/fpm ./data/_lvl_pc/gal ./data/_lvl_pc/geo ./data/_lvl_pc/hot ./data/_lvl_pc/kam ./data/_lvl_pc/kas ./data/_lvl_pc/kor ./data/_lvl_pc/load ./data/_lvl_pc/mus ./data/_lvl_pc/myg ./data/_lvl_pc/nab ./data/_lvl_pc/pol ./data/_lvl_pc/rhn ./data/_lvl_pc/shell ./data/_lvl_pc/side ./data/_lvl_pc/sound ./data/_lvl_pc/spa ./data/_lvl_pc/tan ./data/_lvl_pc/tat ./data/_lvl_pc/test ./data/_lvl_pc/uta ./data/_lvl_pc/yav'
+ARCHIVE_GAME_DATA_FILES='data'
 
 DATA_DIRS='./savegames'
 
 APP_MAIN_TYPE='wine'
 APP_MAIN_EXE='battlefrontii.exe'
 APP_MAIN_ICON='battlefrontii.exe'
-APP_MAIN_ICON_RES='16 32'
 
-PACKAGES_LIST='PKG_MOVIES PKG_DATA PKG_BIN'
+PACKAGES_LIST='PKG_MOVIES PKG_BIN PKG_DATA'
 
 PKG_MOVIES_ID="${GAME_ID}-movies"
 PKG_MOVIES_DESCRIPTION='movies'
@@ -91,30 +88,30 @@ PKG_BIN_DEPS_ARCH='lib32-libtxc_dxtn'
 
 # Load common functions
 
-target_version='2.5'
+target_version='2.10'
 
 if [ -z "$PLAYIT_LIB2" ]; then
-	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
-	if [ -e "$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh" ]; then
-		PLAYIT_LIB2="$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh"
-	elif [ -e './libplayit2.sh' ]; then
-		PLAYIT_LIB2='./libplayit2.sh'
-	else
-		printf '\n\033[1;31mError:\033[0m\n'
-		printf 'libplayit2.sh not found.\n'
-		exit 1
-	fi
+	: ${XDG_DATA_HOME:="$HOME/.local/share"}
+	for path in\
+		"$PWD"\
+		"$XDG_DATA_HOME/play.it"\
+		'/usr/local/share/games/play.it'\
+		'/usr/local/share/play.it'\
+		'/usr/share/games/play.it'\
+		'/usr/share/play.it'
+	do
+		if [ -e "$path/libplayit2.sh" ]; then
+			PLAYIT_LIB2="$path/libplayit2.sh"
+			break
+		fi
+	done
+fi
+if [ -z "$PLAYIT_LIB2" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'libplayit2.sh not found.\n'
+	exit 1
 fi
 . "$PLAYIT_LIB2"
-
-# Check that all parts of the installer are present
-
-ARCHIVE_MAIN="$ARCHIVE"
-set_archive 'ARCHIVE_PART1' "${ARCHIVE_MAIN}_PART1"
-[ "$ARCHIVE_PART1" ] || set_archive_error_not_found "${ARCHIVE_MAIN}_PART1"
-set_archive 'ARCHIVE_PART2' "${ARCHIVE_MAIN}_PART2"
-[ "$ARCHIVE_PART2" ] || set_archive_error_not_found "${ARCHIVE_MAIN}_PART2"
-ARCHIVE="$ARCHIVE_MAIN"
 
 # Extract game data
 
@@ -122,17 +119,14 @@ ln --symbolic "$(readlink --canonicalize "$ARCHIVE_PART1")" "$PLAYIT_WORKDIR/$GA
 ln --symbolic "$(readlink --canonicalize "$ARCHIVE_PART2")" "$PLAYIT_WORKDIR/$GAME_ID.r01"
 extract_data_from "$PLAYIT_WORKDIR/$GAME_ID.r00"
 tolower "$PLAYIT_WORKDIR/gamedata"
+prepare_package_layout
+rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
-for PKG in $PACKAGES_LIST; do
-	organize_data "DOC_${PKG#PKG_}"  "$PATH_DOC"
-	organize_data "GAME_${PKG#PKG_}" "$PATH_GAME"
-done
+# Estract icons
 
 PKG='PKG_BIN'
-extract_and_sort_icons_from 'APP_MAIN'
-move_icons_to 'PKG_DATA'
-
-rm --recursive "$PLAYIT_WORKDIR/gamedata"
+icons_get_from_package 'APP_MAIN'
+icons_move_to 'PKG_DATA'
 
 # Write launchers
 
