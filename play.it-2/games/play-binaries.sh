5f69dd0a50862f3bc7ab0a938907df9d13090f5f
vv221 <vv221@dotslashplay.it>
2018-10-01 20:26:09 +0200

    Binaries — Drop dependency on PulseAudio

--- play.it-2/games/play-binaries.sh
+++ play.it-2/games/play-binaries.sh
@@ -35,7 +35,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20181001.1
+script_version=20181001.2
 
 # Set game-specific variables
 
@@ -60,7 +60,6 @@ ARCHIVE_GAME_DATA_FILES='*_Data'
 DATA_DIRS='./logs'
 
 APP_MAIN_TYPE='native'
-APP_MAIN_PRERUN='pulseaudio --start'
 APP_MAIN_EXE_BIN32='Binaries.x86'
 APP_MAIN_EXE_BIN64='Binaries.x86_64'
 APP_MAIN_OPTIONS='-logFile ./logs/$(date +%F-%R).log'
@@ -72,7 +71,7 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN32_ARCH='32'
-PKG_BIN32_DEPS="$PKG_DATA_ID glibc libstdc++ glx xcursor libxrandr pulseaudio"
+PKG_BIN32_DEPS="$PKG_DATA_ID glibc libstdc++ glx xcursor libxrandr"
 
 PKG_BIN64_ARCH='64'
 PKG_BIN64_DEPS="$PKG_BIN32_DEPS"
