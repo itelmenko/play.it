e0b0a1e72e48b50565627aa10f0ce6bbf96fc672
vv221 <vv221@dotslashplay.it>
2018-10-02 21:06:00 +0200

    Pan Pan — Drop dependency on PulseAudio

--- play.it-2/games/play-pan-pan.sh
+++ play.it-2/games/play-pan-pan.sh
@@ -35,7 +35,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20181002.1
+script_version=20181002.2
 
 # Set game-specific variables
 
@@ -63,7 +63,17 @@ ARCHIVE_GAME_DATA_FILES='PAN-PAN_Data'
 DATA_DIRS='./logs'
 
 APP_MAIN_TYPE='native'
-APP_MAIN_PRERUN='pulseaudio --start'
+APP_MAIN_PRERUN='if ! command -v pulseaudio >/dev/null 2>&1; then
+	mkdir --parents libs
+	ln --force --symbolic /dev/null libs/libpulse-simple.so.0
+	export LD_LIBRARY_PATH="libs:$LD_LIBRARY_PATH"
+else
+	if [ -e "libs/libpulse-simple.so.0" ]; then
+		rm libs/libpulse-simple.so.0
+		rmdir --ignore-fail-on-non-empty libs
+	fi
+	pulseaudio --start
+fi'
 APP_MAIN_EXE='PAN-PAN.x86_64'
 APP_MAIN_OPTIONS='-logFile ./logs/$(date +%F-%R).log'
 APP_MAIN_ICON='PAN-PAN_Data/Resources/UnityPlayer.png'
@@ -74,7 +84,7 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN_ARCH='64'
-PKG_BIN_DEPS="$PKG_DATA_ID glibc libstdc++ xcursor glx libxrandr pulseaudio"
+PKG_BIN_DEPS="$PKG_DATA_ID glibc libstdc++ xcursor glx libxrandr"
 
 # Load common functions
 
