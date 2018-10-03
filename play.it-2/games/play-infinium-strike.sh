5a70debc2c38c02b4805837a5fd49e2f30a49708
vv221 <vv221@dotslashplay.it>
2018-10-03 13:09:19 +0200

    Infinium Strike — Add missing dependency on libudev1

--- play.it-2/games/play-infinium-strike.sh
+++ play.it-2/games/play-infinium-strike.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20181003.1
+script_version=20181003.2
 
 # Set game-specific variables
 
@@ -78,7 +78,7 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN_ARCH='32'
-PKG_BIN_DEPS="$PKG_DATA_ID glibc libstdc++ glx xcursor libxrandr pulseaudio"
+PKG_BIN_DEPS="$PKG_DATA_ID glibc libstdc++ glx xcursor libxrandr libudev1 pulseaudio"
 
 # Load common functions
 
