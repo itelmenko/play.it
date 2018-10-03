995ab03f1d04f89dfb1200acbc395e7d8a2c0954
vv221 <vv221@dotslashplay.it>
2018-10-03 12:51:06 +0200

    Anomaly Defenders — Update script to 2.10 syntax

--- play.it-2/games/play-anomaly-defenders.sh
+++ play.it-2/games/play-anomaly-defenders.sh
@@ -30,18 +30,16 @@ set -o errexit
 
 ###
 # Anomaly Defenders
-# build native Linux packages from the original installers
+# build native packages from the original installers
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20180224.1
+script_version=20181003.1
 
 # Set game-specific variables
 
 GAME_ID='anomaly-defenders'
 GAME_NAME='Anomaly Defenders'
-
-ARCHIVES_LIST='ARCHIVE_HUMBLE'
 
 ARCHIVE_HUMBLE='AnomalyDefenders_Linux_1402512837.tar.gz'
 ARCHIVE_HUMBLE_URL='https://www.humblebundle.com/store/anomaly-defenders'
@@ -50,47 +48,53 @@ ARCHIVE_HUMBLE_SIZE='640000'
 ARCHIVE_HUMBLE_VERSION='1.0-humble'
 
 ARCHIVE_DOC_DATA_PATH='AnomalyDefenders'
-ARCHIVE_DOC_DATA_FILES='./Copyright?license* ./README'
+ARCHIVE_DOC_DATA_FILES='Copyright?license* README'
 
 ARCHIVE_GAME_BIN_PATH='AnomalyDefenders'
-ARCHIVE_GAME_BIN_FILES='./AnomalyDefenders ./libOpenAL.so'
+ARCHIVE_GAME_BIN_FILES='AnomalyDefenders libOpenAL.so'
 
 ARCHIVE_GAME_DATA_PATH='AnomalyDefenders'
-ARCHIVE_GAME_DATA_FILES='./*.dat ./*.idx ./icon.png'
+ARCHIVE_GAME_DATA_FILES='*.dat *.idx icon.png'
 
 APP_MAIN_TYPE='native'
 APP_MAIN_PRERUN='gcc -m32 -o preload.so preload.c -ldl -shared -fPIC -Wall -Wextra
 pulseaudio --start
 export LD_PRELOAD=./preload.so'
 APP_MAIN_EXE='AnomalyDefenders'
-APP_MAIN_ICONS_LIST='APP_MAIN_ICON'
 APP_MAIN_ICON='icon.png'
-APP_MAIN_ICON_RES='64'
 
-PACKAGES_LIST='PKG_DATA PKG_BIN'
+PACKAGES_LIST='PKG_BIN PKG_DATA'
 
 PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTIOn='data'
 
 PKG_BIN_ARCH='32'
-PKG_BIN_DEPS_DEB="$PKG_DATA_ID, libc6, libstdc++6, liblg1-mesa-glx | libgl1, pulseaudio:amd64 | pulseaudio, gcc-multilib:amd64 | gcc, libc6-dev-i386:amd64 | libc6-dev"
-PKG_BIN_DEPS_ARCH="$PKG_DATA_ID lib32-glibc lib32-gcc-libs lib32-libgl pulseaudio gcc-multilib"
+PKG_BIN_DEPS="$PKG_DATA_ID glibc libstdc++ glx gcc32 pulseaudio"
 
 # Load common functions
 
-target_version='2.3'
+target_version='2.10'
 
 if [ -z "$PLAYIT_LIB2" ]; then
-	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
-	if [ -e "$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh" ]; then
-		PLAYIT_LIB2="$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh"
-	elif [ -e './libplayit2.sh' ]; then
-		PLAYIT_LIB2='./libplayit2.sh'
-	else
-		printf '\n\033[1;31mError:\033[0m\n'
-		printf 'libplayit2.sh not found.\n'
-		exit 1
-	fi
+	: ${XDG_DATA_HOME:="$HOME/.local/share"}
+	for path in\
+		"$PWD"\
+		"$XDG_DATA_HOME/play.it"\
+		'/usr/local/share/games/play.it'\
+		'/usr/local/share/play.it'\
+		'/usr/share/games/play.it'\
+		'/usr/share/play.it'
+	do
+		if [ -e "$path/libplayit2.sh" ]; then
+			PLAYIT_LIB2="$path/libplayit2.sh"
+			break
+		fi
+	done
+fi
+if [ -z "$PLAYIT_LIB2" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'libplayit2.sh not found.\n'
+	exit 1
 fi
 . "$PLAYIT_LIB2"
 
@@ -98,12 +102,7 @@ fi
 
 extract_data_from "$SOURCE_ARCHIVE"
 set_standard_permissions "$PLAYIT_WORKDIR/gamedata"
-
-for PKG in $PACKAGES_LIST; do
-	organize_data "DOC_${PKG#PKG_}"  "$PATH_DOC"
-	organize_data "GAME_${PKG#PKG_}" "$PATH_GAME"
-done
-
+prepare_package_layout
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Write launchers
@@ -137,7 +136,8 @@ EOF
 
 # Build package
 
-postinst_icons_linking 'APP_MAIN'
+PKG='PKG_DATA'
+icons_linking_postinst 'APP_MAIN'
 write_metadata 'PKG_DATA'
 write_metadata 'PKG_BIN'
 build_pkg
