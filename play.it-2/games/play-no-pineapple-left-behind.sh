35609c56e5b166a002f94fdb07d69b25ebbdce2f
vv221 <vv221@dotslashplay.it>
2018-10-04 22:44:50 +0200

    No Pineapple Left Behind — Add missing dependencies

--- play.it-2/games/play-no-pineapple-left-behind.sh
+++ play.it-2/games/play-no-pineapple-left-behind.sh
@@ -35,7 +35,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20181004.1
+script_version=20181004.2
 
 # Set game-specific variables
 
@@ -75,7 +75,7 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN32_ARCH='32'
-PKG_BIN32_DEPS="$PKG_DATA_ID glibc libstdc++ glx xcursor pulseaudio"
+PKG_BIN32_DEPS="$PKG_DATA_ID glibc libstdc++ glx xcursor libxrandr libudev1 gtk2 pulseaudio"
 
 PKG_BIN64_ARCH='64'
 PKG_BIN64_DEPS="$PKG_BIN32_DEPS"
