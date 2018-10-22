cf9d869caf55ac323b8cac34add33927e02de401
vv221 <vv221@dotslashplay.it>
2018-10-22 19:46:01 +0200

    Moon Hunters — Add missing dependency on libudev

--- play.it-2/games/play-moon-hunters.sh
+++ play.it-2/games/play-moon-hunters.sh
@@ -35,7 +35,7 @@ set -o errexit
 # send your bug reports to mopi@dotslashplay.it
 ###
 
-script_version=20180917.1
+script_version=20181022.1
 
 # Set game-specific variables
 
@@ -74,7 +74,7 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN32_ARCH='32'
-PKG_BIN32_DEPS="$PKG_DATA_ID glibc libstdc++ glx xcursor libxrandr pulseaudio"
+PKG_BIN32_DEPS="$PKG_DATA_ID glibc libstdc++ glx xcursor libxrandr libudev1 pulseaudio"
 
 PKG_BIN64_ARCH='64'
 PKG_BIN64_DEPS="$PKG_BIN32_DEPS"
