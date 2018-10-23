1abdf32581de89d87d8ea95d98b4baef61c303b1
vv221 <vv221@dotslashplay.it>
2018-10-23 22:31:19 +0200

    Faster Than Light — Syntax updates

--- play.it-2/games/play-faster-than-light.sh
+++ play.it-2/games/play-faster-than-light.sh
@@ -35,7 +35,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20181014.1
+script_version=20181023.1
 
 # Set game-specific variables
 
@@ -49,11 +49,11 @@ ARCHIVE_GOG_SIZE='230000'
 ARCHIVE_GOG_VERSION='1.6.8-gog24110'
 ARCHIVE_GOG_TYPE='mojosetup'
 
-ARCHIVE_GOG_OLD0='ftl_advanced_edition_en_1_6_7_18662.sh'
-ARCHIVE_GOG_OLD0_MD5='2c5254547639b7718dac7a06dabd1d82'
-ARCHIVE_GOG_OLD0_SIZE='210000'
-ARCHIVE_GOG_OLD0_VERSION='1.6.7-gog18662'
-ARCHIVE_GOG_OLD0_TYPE='mojosetup'
+ARCHIVE_GOG_OLD2='ftl_advanced_edition_en_1_6_7_18662.sh'
+ARCHIVE_GOG_OLD2_MD5='2c5254547639b7718dac7a06dabd1d82'
+ARCHIVE_GOG_OLD2_SIZE='210000'
+ARCHIVE_GOG_OLD2_VERSION='1.6.7-gog18662'
+ARCHIVE_GOG_OLD2_TYPE='mojosetup'
 
 ARCHIVE_GOG_OLD1='ftl_advanced_edition_en_1_6_3_17917.sh'
 ARCHIVE_GOG_OLD1_MD5='b64692d5302a1ab60d912c5eb5fbc5e4'
@@ -61,10 +61,10 @@ ARCHIVE_GOG_OLD1_SIZE='210000'
 ARCHIVE_GOG_OLD1_VERSION='1.6.3-gog17917'
 ARCHIVE_GOG_OLD1_TYPE='mojosetup'
 
-ARCHIVE_GOG_OLD2='gog_ftl_advanced_edition_2.0.0.2.sh'
-ARCHIVE_GOG_OLD2_MD5='2c24b70b31316acefedc082e9441a69a'
-ARCHIVE_GOG_OLD2_SIZE='220000'
-ARCHIVE_GOG_OLD2_VERSION='1.5.13-gog2.0.0.2'
+ARCHIVE_GOG_OLD0='gog_ftl_advanced_edition_2.0.0.2.sh'
+ARCHIVE_GOG_OLD0_MD5='2c24b70b31316acefedc082e9441a69a'
+ARCHIVE_GOG_OLD0_SIZE='220000'
+ARCHIVE_GOG_OLD0_VERSION='1.5.13-gog2.0.0.2'
 
 ARCHIVE_HUMBLE='FTL.1.5.13.tar.gz'
 ARCHIVE_HUMBLE_URL='https://www.humblebundle.com/store/ftl-faster-than-light'
@@ -72,48 +72,55 @@ ARCHIVE_HUMBLE_MD5='791e0bc8de73fcdcd5f461a4548ea2d8'
 ARCHIVE_HUMBLE_SIZE='220000'
 ARCHIVE_HUMBLE_VERSION='1.5.13-humble140602'
 
-ARCHIVE_DOC1_PATH_GOG='data/noarch/docs'
-ARCHIVE_DOC1_PATH_HUMBLE='FTL'
-ARCHIVE_DOC1_FILES='./*.html ./*.txt'
+ARCHIVE_DOC0_DATA_PATH_GOG='data/noarch/docs'
+ARCHIVE_DOC0_DATA_PATH_HUMBLE='FTL'
+ARCHIVE_DOC0_DATA_FILES='*.html *.txt'
 
-ARCHIVE_DOC2_PATH_GOG='data/noarch/game/data'
-ARCHIVE_DOC2_PATH_HUMBLE='FTL/data'
-ARCHIVE_DOC2_FILES='./licenses'
+ARCHIVE_DOC1_DATA_PATH_GOG='data/noarch/game/data'
+ARCHIVE_DOC1_DATA_PATH_HUMBLE='FTL/data'
+ARCHIVE_DOC1_DATA_FILES='licenses'
 
 ARCHIVE_GAME_BIN32_PATH_GOG='data/noarch/game/data'
 ARCHIVE_GAME_BIN32_PATH_HUMBLE='FTL/data'
-ARCHIVE_GAME_BIN32_FILES_GOG='./FTL.x86'
-ARCHIVE_GAME_BIN32_FILES_GOG_OLD2='./x86'
-ARCHIVE_GAME_BIN32_FILES_HUMBLE='./x86'
+ARCHIVE_GAME_BIN32_FILES_GOG='FTL.x86'
+ARCHIVE_GAME_BIN32_FILES_HUMBLE='x86'
+# Keep compatibility with old archives
+ARCHIVE_GAME_BIN32_FILES_GOG_OLD0='x86'
 
 ARCHIVE_GAME_BIN64_PATH_GOG='data/noarch/game/data'
 ARCHIVE_GAME_BIN64_PATH_HUMBLE='FTL/data'
-ARCHIVE_GAME_BIN64_FILES_GOG='./FTL.amd64'
-ARCHIVE_GAME_BIN64_FILES_GOG_OLD2='./amd64'
-ARCHIVE_GAME_BIN64_FILES_HUMBLE='./amd64'
+ARCHIVE_GAME_BIN64_FILES_GOG='FTL.amd64'
+ARCHIVE_GAME_BIN64_FILES_HUMBLE='amd64'
+# Keep compatibility with old archives
+ARCHIVE_GAME_BIN64_FILES_GOG_OLD0='amd64'
 
 ARCHIVE_GAME_DATA_PATH_GOG='data/noarch/game/data'
 ARCHIVE_GAME_DATA_PATH_HUMBLE='FTL/data'
-ARCHIVE_GAME_DATA_FILES_GOG='./exe_icon.bmp ./ftl.dat'
-ARCHIVE_GAME_DATA_FILES_GOG_OLD2='./exe_icon.bmp ./resources'
-ARCHIVE_GAME_DATA_FILES_HUMBLE='./exe_icon.bmp ./resources'
+ARCHIVE_GAME_DATA_FILES_GOG='exe_icon.bmp ftl.dat'
+ARCHIVE_GAME_DATA_FILES_HUMBLE='exe_icon.bmp resources'
+# Keep compatibility with old archives
+ARCHIVE_GAME_DATA_FILES_GOG_OLD0='exe_icon.bmp resources'
 
 APP_MAIN_TYPE='native'
 APP_MAIN_PRERUN='export LANG=C'
-APP_MAIN_EXE_BIN32_GOG='FTL.x86'
-APP_MAIN_EXE_BIN32_GOG_OLD2='x86/bin/FTL'
-APP_MAIN_EXE_BIN32_HUMBLE='x86/bin/FTL'
-APP_MAIN_EXE_BIN64_GOG='FTL.amd64'
-APP_MAIN_EXE_BIN64_GOG_OLD2='amd64/bin/FTL'
-APP_MAIN_EXE_BIN64_HUMBLE='amd64/bin/FTL'
-APP_MAIN_LIBS_BIN32_GOG_OLD2='x86/lib'
+APP_MAIN_ICON0='exe_icon.bmp'
+APP_MAIN_ICON1='resources/exe_icon.bmp'
+
 APP_MAIN_LIBS_BIN32_HUMBLE='x86/lib'
-APP_MAIN_LIBS_BIN64_GOG_OLD2='amd64/lib'
 APP_MAIN_LIBS_BIN64_HUMBLE='amd64/lib'
-APP_MAIN_ICON1='exe_icon.bmp'
-APP_MAIN_ICON1_RES='64'
-APP_MAIN_ICON2='resources/exe_icon.bmp'
-APP_MAIN_ICON2_RES='32'
+APP_MAIN_EXE_BIN32_HUMBLE='x86/bin/FTL'
+APP_MAIN_EXE_BIN64_HUMBLE='amd64/bin/FTL'
+APP_MAIN_ICONS_LIST_HUMBLE='APP_MAIN_ICON0 APP_MAIN_ICON1'
+
+APP_MAIN_EXE_BIN32_GOG='FTL.x86'
+APP_MAIN_EXE_BIN64_GOG='FTL.amd64'
+APP_MAIN_ICONS_LIST_GOG='APP_MAIN_ICON0'
+# Keep compatibility with old archives
+APP_MAIN_LIBS_BIN32_GOG_OLD0='x86/lib'
+APP_MAIN_LIBS_BIN64_GOG_OLD0='amd64/lib'
+APP_MAIN_EXE_BIN32_GOG_OLD0='x86/bin/FTL'
+APP_MAIN_EXE_BIN64_GOG_OLD0='amd64/bin/FTL'
+APP_MAIN_ICONS_LIST_GOG_OLD0='APP_MAIN_ICON0 APP_MAIN_ICON1'
 
 PACKAGES_LIST='PKG_DATA PKG_BIN32 PKG_BIN64'
 
@@ -122,53 +129,59 @@ PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN32_ARCH='32'
 PKG_BIN32_DEPS_GOG="$PKG_DATA_ID glibc glx alsa"
-PKG_BIN32_DEPS_GOG_OLD2="$PKG_DATA_ID sdl glu"
 PKG_BIN32_DEPS_HUMBLE="$PKG_DATA_ID sdl glu"
+# Keep compatibility with old archives
+PKG_BIN32_DEPS_GOG_OLD0="$PKG_DATA_ID sdl glu"
 
 PKG_BIN64_ARCH='64'
 PKG_BIN64_DEPS_GOG="$PKG_BIN32_DEPS_GOG"
-PKG_BIN64_DEPS_GOG_OLD2="$PKG_BIN32_DEPS_GOG_OLDER"
 PKG_BIN64_DEPS_HUMBLE="$PKG_BIN32_DEPS_HUMBLE"
+# Keep compatibility with old archives
+PKG_BIN64_DEPS_GOG_OLD0="$PKG_BIN32_DEPS_GOG_OLD0"
 
 # Load common functions
 
-target_version='2.7'
+target_version='2.10'
 
 if [ -z "$PLAYIT_LIB2" ]; then
-	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
-	if [ -e "$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh" ]; then
-		PLAYIT_LIB2="$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh"
-	elif [ -e './libplayit2.sh' ]; then
-		PLAYIT_LIB2='./libplayit2.sh'
-	else
-		printf '\n\033[1;31mError:\033[0m\n'
-		printf 'libplayit2.sh not found.\n'
-		exit 1
-	fi
+	: ${XDG_DATA_HOME:="$HOME/.local/share"}
+	for path in\
+		"$PWD"\
+		"$XDG_DATA_HOME/play.it"\
+		'/usr/local/share/games/play.it'\
+		'/usr/local/share/play.it'\
+		'/usr/share/games/play.it'\
+		'/usr/share/play.it'
+	do
+		if [ -e "$path/libplayit2.sh" ]; then
+			PLAYIT_LIB2="$path/libplayit2.sh"
+			break
+		fi
+	done
+fi
+if [ -z "$PLAYIT_LIB2" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'libplayit2.sh not found.\n'
+	exit 1
 fi
 . "$PLAYIT_LIB2"
 
 # Extract game data
 
 extract_data_from "$SOURCE_ARCHIVE"
-set_standard_permissions "$PLAYIT_WORKDIR/gamedata"
+case "$archive" in
+	('ARCHIVE_HUMBLE')
+		set_standard_permissions "$PLAYIT_WORKDIR/gamedata"
+	;;
+esac
 prepare_package_layout
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Extract icons
 
-extract_icon_from "${PKG_DATA_PATH}${PATH_GAME}/$APP_MAIN_ICON1"
-res="$APP_MAIN_ICON1_RES"
-PATH_ICON="$PATH_ICON_BASE/${res}x${res}/apps"
-mkdir --parents "${PKG_DATA_PATH}${PATH_ICON}"
-mv "$PLAYIT_WORKDIR/icons/${APP_MAIN_ICON1%.bmp}.png" "${PKG_DATA_PATH}${PATH_ICON}/$GAME_ID.png"
-if [ -f "${PKG_DATA_PATH}${PATH_GAME}/$APP_MAIN_ICON2" ]; then
-	extract_icon_from "${PKG_DATA_PATH}${PATH_GAME}/$APP_MAIN_ICON2"
-	res="$APP_MAIN_ICON2_RES"
-	PATH_ICON="$PATH_ICON_BASE/${res}x${res}/apps"
-	mkdir --parents "${PKG_DATA_PATH}${PATH_ICON}"
-	mv "$PLAYIT_WORKDIR/icons/$(basename "$APP_MAIN_ICON2" .bmp).png" "${PKG_DATA_PATH}${PATH_ICON}/$GAME_ID.png"
-fi
+PKG='PKG_DATA'
+use_archive_specific_value 'APP_MAIN_ICONS_LIST'
+icons_get_from_package 'APP_MAIN'
 
 # Write launchers
 
