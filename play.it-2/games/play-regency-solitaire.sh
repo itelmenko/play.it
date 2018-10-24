ccbfee2f7742099b0a6db63b83ddd3c882c7b774
vv221 <vv221@dotslashplay.it>
2018-10-24 16:46:28 +0200

    Regency Solitaire — Update authors list + Update script to 2.10 syntax

--- play.it-2/games/play-regency-solitaire.sh
+++ play.it-2/games/play-regency-solitaire.sh
@@ -3,6 +3,7 @@ set -o errexit
 
 ###
 # Copyright (c) 2015-2018, Antoine Le Gonidec
+# Copyright (c) 2017-2018, Solène Huault
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -30,18 +31,16 @@ set -o errexit
 
 ###
 # Regency Solitaire
-# build native Linux packages from the original installers
+# build native packages from the original installers
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20180224.1
+script_version=20181024.1
 
 # Set game-specific variables
 
 GAME_ID='regency-solitaire'
 GAME_NAME='Regency Solitaire'
-
-ARCHIVES_LIST='ARCHIVE_HUMBLE'
 
 ARCHIVE_HUMBLE='RegencySolitaireV117b_1472495785.tar.gz'
 ARCHIVE_HUMBLE_URL='https://www.humblebundle.com/store/regency-solitaire'
@@ -50,10 +49,10 @@ ARCHIVE_HUMBLE_SIZE='71000'
 ARCHIVE_HUMBLE_VERSION='1.17b-humble160829'
 
 ARCHIVE_GAME_BIN_PATH='RegencySolitaireV117b'
-ARCHIVE_GAME_BIN_FILES='./RegencySolitaire'
+ARCHIVE_GAME_BIN_FILES='RegencySolitaire'
 
 ARCHIVE_GAME_DATA_PATH='RegencySolitaireV117b'
-ARCHIVE_GAME_DATA_FILES='./data'
+ARCHIVE_GAME_DATA_FILES='data'
 
 APP_MAIN_TYPE='native'
 APP_MAIN_EXE='RegencySolitaire'
@@ -64,24 +63,32 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN_ARCH='64'
-PKG_BIN_DEPS_DEB="$PKG_DATA_ID, libc6, libstdc++6, libglu1-mesa | libglu1"
-PKG_BIN_DEPS_ARCH="$PKG_DATA_ID glu"
+PKG_BIN_DEPS="$PKG_DATA_ID glibc libstdc++ glu"
 
 # Load common functions
 
-target_version='2.0'
+target_version='2.10'
 
 if [ -z "$PLAYIT_LIB2" ]; then
-	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
-	if [ -e "$XDG_DATA_HOME/play.it/libplayit2.sh" ]; then
-		PLAYIT_LIB2="$XDG_DATA_HOME/play.it/libplayit2.sh"
-	elif [ -e './libplayit2.sh' ]; then
-		PLAYIT_LIB2='./libplayit2.sh'
-	else
-		printf '\n\033[1;31mError:\033[0m\n'
-		printf 'libplayit2.sh not found.\n'
-		exit 1
-	fi
+	: ${XDG_DATA_HOME:="$HOME/.local/share"}
+	for path in\
+		"$PWD"\
+		"$XDG_DATA_HOME/play.it"\
+		'/usr/local/share/games/play.it'\
+		'/usr/local/share/play.it'\
+		'/usr/share/games/play.it'\
+		'/usr/share/play.it'
+	do
+		if [ -e "$path/libplayit2.sh" ]; then
+			PLAYIT_LIB2="$path/libplayit2.sh"
+			break
+		fi
+	done
+fi
+if [ -z "$PLAYIT_LIB2" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'libplayit2.sh not found.\n'
+	exit 1
 fi
 . "$PLAYIT_LIB2"
 
@@ -89,13 +96,7 @@ fi
 
 extract_data_from "$SOURCE_ARCHIVE"
 set_standard_permissions "$PLAYIT_WORKDIR/gamedata"
-
-PKG='PKG_BIN'
-organize_data 'GAME_BIN' "$PATH_GAME"
-
-PKG='PKG_DATA'
-organize_data 'GAME_DATA' "$PATH_GAME"
-
+prepare_package_layout
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Write launchers
