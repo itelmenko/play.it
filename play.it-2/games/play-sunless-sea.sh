be9a45a3238e071ad788f9046f3c0263cce648a0
vv221 <vv221@dotslashplay.it>
2018-11-01 19:41:13 +0100

    Sunless Sea — Update authors list

--- play.it-2/games/play-sunless-sea.sh
+++ play.it-2/games/play-sunless-sea.sh
@@ -3,6 +3,7 @@ set -o errexit
 
 ###
 # Copyright (c) 2015-2018, Antoine Le Gonidec
+# Copyright (c) 2018, BetaRays
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
