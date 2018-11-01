8559f4810199e4abe43c4708752910a120a56607
vv221 <vv221@dotslashplay.it>
2018-11-01 19:46:38 +0100

    Sunless Sea — Update script to 2.10 syntax

--- play.it-2/games/play-sunless-sea.sh
+++ play.it-2/games/play-sunless-sea.sh
@@ -35,14 +35,14 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20180610.1
+script_version=20181111.2
 
 # Set game-specific variables
 
 GAME_ID='sunless-sea'
 GAME_NAME='Sunless Sea'
 
-ARCHIVES_LIST='ARCHIVE_GOG_ZUBMARINER ARCHIVE_GOG_ZUBMARINER_OLD ARCHIVE_GOG ARCHIVE_GOG_OLD ARCHIVE_GOG_OLD0 ARCHIVE_HUMBLE ARCHIVE_HUMBLE_OLD'
+ARCHIVES_LIST='ARCHIVE_GOG_ZUBMARINER ARCHIVE_GOG_ZUBMARINER_OLD0 ARCHIVE_GOG ARCHIVE_GOG_OLD1 ARCHIVE_GOG_OLD0 ARCHIVE_HUMBLE ARCHIVE_HUMBLE_OLD0'
 
 ARCHIVE_GOG_ZUBMARINER='sunless_sea_zubmariner_en_v2_2_4_3141_21326.sh'
 ARCHIVE_GOG_ZUBMARINER_URL='https://www.gog.com/game/sunless_sea_zubmariner'
@@ -51,10 +51,10 @@ ARCHIVE_GOG_ZUBMARINER_VERSION='2.2.4.3141-gog21326'
 ARCHIVE_GOG_ZUBMARINER_TYPE='mojosetup'
 ARCHIVE_GOG_ZUBMARINER_SIZE='930000'
 
-ARCHIVE_GOG_ZUBMARINER_OLD='gog_sunless_sea_zubmariner_2.5.0.6.sh'
-ARCHIVE_GOG_ZUBMARINER_OLD_MD5='692cd0dac832d5254bd38d7e1a05b918'
-ARCHIVE_GOG_ZUBMARINER_OLD_VERSION='2.2.2.3130-gog2.5.0.6'
-ARCHIVE_GOG_ZUBMARINER_OLD_SIZE='870000'
+ARCHIVE_GOG_ZUBMARINER_OLD0='gog_sunless_sea_zubmariner_2.5.0.6.sh'
+ARCHIVE_GOG_ZUBMARINER_OLD0_MD5='692cd0dac832d5254bd38d7e1a05b918'
+ARCHIVE_GOG_ZUBMARINER_OLD0_VERSION='2.2.2.3130-gog2.5.0.6'
+ARCHIVE_GOG_ZUBMARINER_OLD0_SIZE='870000'
 
 ARCHIVE_GOG='sunless_sea_2_2_6_3150_24613.sh'
 ARCHIVE_GOG_URL='https://www.gog.com/game/sunless_sea'
@@ -63,11 +63,11 @@ ARCHIVE_GOG_VERSION='2.2.6.3150-gog24613'
 ARCHIVE_GOG_TYPE='mojosetup'
 ARCHIVE_GOG_SIZE='760000'
 
-ARCHIVE_GOG_OLD='sunless_sea_en_v2_2_4_3141_21326.sh'
-ARCHIVE_GOG_OLD_MD5='df453a83ac1fb2767bdeafafb40f037a'
-ARCHIVE_GOG_OLD_VERSION='2.2.4.3141-gog21326'
-ARCHIVE_GOG_OLD_TYPE='mojosetup'
-ARCHIVE_GOG_OLD_SIZE='760000'
+ARCHIVE_GOG_OLD1='sunless_sea_en_v2_2_4_3141_21326.sh'
+ARCHIVE_GOG_OLD1_MD5='df453a83ac1fb2767bdeafafb40f037a'
+ARCHIVE_GOG_OLD1_VERSION='2.2.4.3141-gog21326'
+ARCHIVE_GOG_OLD1_TYPE='mojosetup'
+ARCHIVE_GOG_OLD1_SIZE='760000'
 
 ARCHIVE_GOG_OLD0='gog_sunless_sea_2.8.0.11.sh'
 ARCHIVE_GOG_OLD0_MD5='1cf6bb7a440ce796abf8e7afcb6f7a54'
@@ -80,35 +80,35 @@ ARCHIVE_HUMBLE_MD5='076c6784bb96e4189f675f114c98ae85'
 ARCHIVE_HUMBLE_VERSION='2.2.4.3141-humble180606'
 ARCHIVE_HUMBLE_SIZE='760000'
 
-ARCHIVE_HUMBLE_OLD='Sunless_Sea_Setup_V2.2.2.3129_LINUX.zip'
-ARCHIVE_HUMBLE_OLD_MD5='bdb37932e56fd0655a2e4263631e2582'
-ARCHIVE_HUMBLE_OLD_VERSION='2.2.2.3129-humble170131'
-ARCHIVE_HUMBLE_OLD_SIZE='700000'
+ARCHIVE_HUMBLE_OLD0='Sunless_Sea_Setup_V2.2.2.3129_LINUX.zip'
+ARCHIVE_HUMBLE_OLD0_MD5='bdb37932e56fd0655a2e4263631e2582'
+ARCHIVE_HUMBLE_OLD0_VERSION='2.2.2.3129-humble170131'
+ARCHIVE_HUMBLE_OLD0_SIZE='700000'
 
 ARCHIVE_DOC0_DATA_PATH_GOG='data/noarch/game'
 ARCHIVE_DOC0_DATA_PATH_HUMBLE='data/noarch'
-ARCHIVE_DOC0_DATA_FILES='./README.linux'
+ARCHIVE_DOC0_DATA_FILES='README.linux'
 
 ARCHIVE_DOC1_DATA_PATH_GOG='data/noarch/docs'
-ARCHIVE_DOC1_DATA_FILES_GOG='./*'
+ARCHIVE_DOC1_DATA_FILES_GOG='*'
 
 ARCHIVE_GAME0_BIN32_PATH_GOG='data/noarch/game'
 ARCHIVE_GAME0_BIN32_PATH_HUMBLE='data/noarch'
-ARCHIVE_GAME0_BIN32_FILES='./*.x86 ./*_Data/*/x86'
+ARCHIVE_GAME0_BIN32_FILES='Sunless?Sea.x86 Sunless?Sea_Data/*/x86'
 
 ARCHIVE_GAME1_BIN32_PATH_HUMBLE='data/x86'
-ARCHIVE_GAME1_BIN32_FILES='./*.x86 ./*_Data/*/x86'
+ARCHIVE_GAME1_BIN32_FILES_HUMBLE='Sunless?Sea.x86 Sunless?Sea_Data/*/x86'
 
 ARCHIVE_GAME0_BIN64_PATH_GOG='data/noarch/game'
 ARCHIVE_GAME0_BIN64_PATH_HUMBLE='data/noarch'
-ARCHIVE_GAME0_BIN64_FILES='./*.x86_64 ./*_Data/*/x86_64'
+ARCHIVE_GAME0_BIN64_FILES='Sunless?Sea.x86_64 Sunless?Sea_Data/*/x86_64'
 
 ARCHIVE_GAME1_BIN64_PATH_HUMBLE='data/x86_64'
-ARCHIVE_GAME1_BIN64_FILES='./*.x86_64 ./*_Data/*/x86_64'
+ARCHIVE_GAME1_BIN64_FILES_HUMBLE='Sunless?Sea.x86_64 Sunless?Sea_Data/*/x86_64'
 
 ARCHIVE_GAME_DATA_PATH_GOG='data/noarch/game'
 ARCHIVE_GAME_DATA_PATH_HUMBLE='data/noarch'
-ARCHIVE_GAME_DATA_FILES='./*'
+ARCHIVE_GAME_DATA_FILES='*'
 
 DATA_DIRS='./logs'
 
@@ -133,41 +133,42 @@ PKG_BIN64_DEPS="$PKG_BIN32_DEPS"
 
 # Load common functions
 
-target_version='2.9'
+target_version='2.10'
 
 if [ -z "$PLAYIT_LIB2" ]; then
-	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
+	: ${XDG_DATA_HOME:="$HOME/.local/share"}
 	for path in\
-		'./'\
-		"$XDG_DATA_HOME/play.it/"\
-		"$XDG_DATA_HOME/play.it/play.it-2/lib/"\
-		'/usr/local/share/games/play.it/'\
-		'/usr/local/share/play.it/'\
-		'/usr/share/games/play.it/'\
-		'/usr/share/play.it/'
+		"$PWD"\
+		"$XDG_DATA_HOME/play.it"\
+		'/usr/local/share/games/play.it'\
+		'/usr/local/share/play.it'\
+		'/usr/share/games/play.it'\
+		'/usr/share/play.it'
 	do
-		if [ -z "$PLAYIT_LIB2" ] && [ -e "$path/libplayit2.sh" ]; then
+		if [ -e "$path/libplayit2.sh" ]; then
 			PLAYIT_LIB2="$path/libplayit2.sh"
 			break
 		fi
 	done
-	if [ -z "$PLAYIT_LIB2" ]; then
-		printf '\n\033[1;31mError:\033[0m\n'
-		printf 'libplayit2.sh not found.\n'
-		exit 1
-	fi
+fi
+if [ -z "$PLAYIT_LIB2" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'libplayit2.sh not found.\n'
+	exit 1
 fi
 . "$PLAYIT_LIB2"
 
 # Extract game data
 
 extract_data_from "$SOURCE_ARCHIVE"
-if [ "$ARCHIVE" = 'ARCHIVE_HUMBLE' ]; then
-	ARCHIVE_HUMBLE_TYPE='mojosetup'
-	archive="$PLAYIT_WORKDIR/gamedata/Sunless Sea.sh"
-	extract_data_from "$archive"
-	rm "$archive"
-fi
+case "$ARCHIVE" in
+	('ARCHIVE_HUMBLE'*)
+		eval ${ARCHIVE}_TYPE=\'mojosetup\'
+		archive="$PLAYIT_WORKDIR/gamedata/Sunless Sea.sh"
+		extract_data_from "$archive"
+		rm "$archive"
+	;;
+esac
 prepare_package_layout
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
