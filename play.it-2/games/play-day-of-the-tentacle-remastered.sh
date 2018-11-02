90fcb480127035cf3a909a11ce740766f23962bf
vv221 <vv221@dotslashplay.it>
2018-11-02 22:23:44 +0100

    Day of the Tentacle Remastered — Add missing game dependencies Fix https://framagit.org/vv221/play.it-games/issues/67

--- play.it-2/games/play-day-of-the-tentacle-remastered.sh
+++ play.it-2/games/play-day-of-the-tentacle-remastered.sh
@@ -35,7 +35,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20181102.1
+script_version=20181102.2
 
 # Set game-specific variables
 
@@ -70,7 +70,7 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN_ARCH='32'
-PKG_BIN_DEPS="$PKG_DATA_ID glibc libstdc++ glx"
+PKG_BIN_DEPS="$PKG_DATA_ID glibc libstdc++ glx libxrandr libudev1 openal"
 
 # Load common functions
 
