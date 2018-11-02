4cb94ade41dfca871ce735036d0032443d751b94
vv221 <vv221@dotslashplay.it>
2018-11-02 22:41:28 +0100

    Shadow Tactics — Drop dependency on GTK2 Fix https://framagit.org/vv221/play.it-games/issues/68

--- play.it-2/games/play-shadow-tactics.sh
+++ play.it-2/games/play-shadow-tactics.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20181102.1
+script_version=20181102.2
 
 # Set game-specific variables
 
@@ -83,7 +83,7 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN_ARCH='32'
-PKG_BIN_DEPS="$PKG_LIGHTING_ID $PKG_DATA_ID glibc libstdc++ glx gtk2 libudev1"
+PKG_BIN_DEPS="$PKG_LIGHTING_ID $PKG_DATA_ID glibc libstdc++ glx libudev1 xcursor libxrandr"
 
 # Load common functions
 
