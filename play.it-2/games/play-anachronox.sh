531e6ec60c3ab5dfd7690cc41322aa04eef744df
vv221 <vv221@dotslashplay.it>
2018-11-22 08:55:35 +0100

    Anachronox — Update authors list

--- play.it-2/games/play-anachronox.sh
+++ play.it-2/games/play-anachronox.sh
@@ -3,6 +3,7 @@ set -o errexit
 
 ###
 # Copyright (c) 2015-2018, Antoine Le Gonidec
+# Copyright (c) 2016-2018, Solène Huault
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
