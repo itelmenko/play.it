f42225fe13c2db17f284083784935e016abd041b
Mopi <mopi@dotslashplay.it>
2018-12-27 23:31:53 +0100

    Wonder Boy: The Dragon’s Trap - New game

---
+++ play.it-2/games/play-wonder-boy.sh
@@ -0,0 +1,137 @@
+#!/bin/sh -e
+set -o errexit
+
+###
+# Copyright (c) 2015-2018, Antoine Le Gonidec
+# Copyright (c) 2018, Solène Huault
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# This software is provided by the copyright holders and contributors "as is"
+# and any express or implied warranties, including, but not limited to, the
+# implied warranties of merchantability and fitness for a particular purpose
+# are disclaimed. In no event shall the copyright holder or contributors be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of the
+# possibility of such damage.
+###
+
+###
+# Wonder Boy: The Dragon’s Trap
+# build native packages from the original installers
+# send your bug reports to mopi@dotslashplay.it
+###
+
+script_version=20181227.1
+
+# Set game-specific variables
+
+GAME_ID='wonder-boy'
+GAME_NAME='Wonder Boy: The Dragon’s Trap'
+
+ARCHIVE_GOG='wonder_boy_the_dragon_s_trap_en_1_03f_02_20817.sh'
+ARCHIVE_GOG_URL='https://www.gog.com/game/wonder_boy_the_dragons_trap'
+ARCHIVE_GOG_MD5='775ac16ded3d2aca7ebcb27c7a2caf48'
+ARCHIVE_GOG_SIZE='1100000'
+ARCHIVE_GOG_VERSION='1.03f.02-gog20817'
+ARCHIVE_GOG_TYPE='mojosetup'
+
+ARCHIVE_DOC0_PATH='data/noarch/docs'
+ARCHIVE_DOC0_FILES='*'
+
+ARCHIVE_DOC1_PATH='data/noarch/game'
+ARCHIVE_DOC1_FILES='Linux.README'
+
+ARCHIVE_GAME_BIN32_PATH='data/noarch/game'
+ARCHIVE_GAME_BIN32_FILES='x86'
+
+ARCHIVE_GAME_BIN64_PATH='data/noarch/game'
+ARCHIVE_GAME_BIN64_FILES='x86_64'
+
+ARCHIVE_GAME_DATA_PATH='data/noarch/game'
+ARCHIVE_GAME_DATA_FILES='bin_pc gamecontrollerdb.txt WonderBoy.png'
+
+APP_MAIN_TYPE='native'
+APP_MAIN_EXE_BIN32='x86/wb.x86'
+APP_MAIN_EXE_BIN64='x86_64/wb.x86_64'
+APP_MAIN_ICON='WonderBoy.png'
+
+PACKAGES_LIST='PKG_BIN32 PKG_BIN64 PKG_DATA'
+
+PKG_DATA_ID="${GAME_ID}-data"
+PKG_DATA_DESCRIPTION='data'
+
+PKG_BIN32_ARCH='32'
+PKG_BIN32_DEPS="$PKG_DATA_ID glibc libstdc++ glx"
+
+PKG_BIN64_ARCH='64'
+PKG_BIN64_DEPS="$PKG_BIN32_DEPS"
+
+# Load common functions
+
+target_version='2.10'
+
+if [ -z "$PLAYIT_LIB2" ]; then
+	: ${XDG_DATA_HOME:="$HOME/.local/share"}
+	for path in\
+		"$PWD"\
+		"$XDG_DATA_HOME/play.it"\
+		'/usr/local/share/games/play.it'\
+		'/usr/local/share/play.it'\
+		'/usr/share/games/play.it'\
+		'/usr/share/play.it'
+	do
+		if [ -e "$path/libplayit2.sh" ]; then
+			PLAYIT_LIB2="$path/libplayit2.sh"
+			break
+		fi
+	done
+fi
+if [ -z "$PLAYIT_LIB2" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'libplayit2.sh not found.\n'
+	exit 1
+fi
+. "$PLAYIT_LIB2"
+
+# Extract game data
+
+extract_data_from "$SOURCE_ARCHIVE"
+prepare_package_layout
+rm --recursive "$PLAYIT_WORKDIR/gamedata"
+
+# Write launchers
+
+for PKG in 'PKG_BIN32' 'PKG_BIN64'; do
+	write_launcher 'APP_MAIN'
+done
+
+# Build package
+
+PKG='PKG_DATA'
+icons_linking_postinst 'APP_MAIN'
+write_metadata 'PKG_DATA'
+write_metadata 'PKG_BIN32' 'PKG_BIN64'
+build_pkg
+# Clean up
+
+rm --recursive "$PLAYIT_WORKDIR"
+
+# Print instructions
+
+print_instructions
+
+exit 0
