91fea7640ddf977747adc915e328901a10acff9a
vv221 <vv221@dotslashplay.it>
2019-02-10 14:52:13 +0100

    Lemmings — Update script to 2.11 syntax

--- play.it-2/games/play-lemmings.sh
+++ play.it-2/games/play-lemmings.sh
@@ -2,8 +2,8 @@
 set -o errexit
 
 ###
-# Copyright (c) 2015-2018, Antoine Le Gonidec
-# Copyright (c) 2018, Dominique Derrier
+# Copyright (c) 2015-2019, Antoine Le Gonidec
+# Copyright (c) 2018-2019, Dominique Derrier
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -31,97 +31,86 @@ set -o errexit
 
 ###
 # Lemmings | floppy drive version
-# build native Linux packages from the original installers
+# build native packages from the original installers
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20180825.0
+script_version=20190210.1
 
 # Set game-specific variables
 
 GAME_ID='lemmings'
 GAME_NAME='Lemmings'
 
-
-SCRIPT_DEPS="7z"
-#ARCHIVE_TYPE='7z'
-
 ARCHIVE_LTF='jeu-00005-lemmings-pcdos.7z'
 ARCHIVE_LTF_TYPE='7z'
 ARCHIVE_LTF_URL='https://www.abandonware-france.org/ltf_abandon/ltf_jeu.php?id=5'
 ARCHIVE_LTF_MD5='9fb084f3e8945e3280ed3058b6d4d851'
-ARCHIVE_LTF_SIZE='796'
+ARCHIVE_LTF_SIZE='800'
 ARCHIVE_LTF_VERSION='1.0-ltf00005'
 
-# DATA_DIRS='' -- no game save
-# GAME_IMAGE='' -- no cd :)
+ARCHIVE_GAME_BIN_PATH='lem'
+ARCHIVE_GAME_BIN_FILES='*.bat *.exe'
 
+ARCHIVE_GAME_DATA_PATH='lem'
+ARCHIVE_GAME_DATA_FILES='*.dat'
 
-ARCHIVE_GAME_BIN_PATH='Lem'
-ARCHIVE_GAME_BIN_FILES="./*.BAT ./*.EXE"
+ARCHIVE_DOC0_DATA_PATH='.'
+ARCHIVE_DOC0_DATA_FILES='*.txt'
 
-ARCHIVE_GAME_DATA_PATH='Lem'
-ARCHIVE_GAME_DATA_FILES="./*.DAT"
-
-ARCHIVE_DOC_DATA_PATH='.'
-ARCHIVE_DOC_DATA_FILES="./Lem/*.DOC ./*.txt"
+ARCHIVE_DOC1_DATA_PATH='lem'
+ARCHIVE_DOC1_DATA_FILES='*.doc'
 
 APP_MAIN_TYPE='dosbox'
 APP_MAIN_EXE='lemmings.bat'
-#APP_MAIN_ICON='' # no icon
-
-ARCHIVE_GAME_DATA_PATH='Lem'
-ARCHIVE_GAME_DATA_FILES='./*.DAT'
 
 PACKAGES_LIST='PKG_BIN PKG_DATA'
+
 PKG_DATA_ID="${GAME_ID}-data"
-PKG_DATA_DESCRIPTION='data for lemmings'
+PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN_ARCH='32'
 PKG_BIN_DEPS="$PKG_DATA_ID dosbox"
 
 # Load common functions
 
-target_version='2.10'
+target_version='2.11'
 
 if [ -z "$PLAYIT_LIB2" ]; then
-	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
+	: "${XDG_DATA_HOME:="$HOME/.local/share"}"
 	for path in\
-		'./'\
-		"$XDG_DATA_HOME/play.it/"\
-		"$XDG_DATA_HOME/play.it/play.it-2/lib/"\
-		'/usr/local/share/games/play.it/'\
-		'/usr/local/share/play.it/'\
-		'/usr/share/games/play.it/'\
-		'/usr/share/play.it/'
+		"$PWD"\
+		"$XDG_DATA_HOME/play.it"\
+		'/usr/local/share/games/play.it'\
+		'/usr/local/share/play.it'\
+		'/usr/share/games/play.it'\
+		'/usr/share/play.it'
 	do
-		if [ -z "$PLAYIT_LIB2" ] && [ -e "$path/libplayit2.sh" ]; then
+		if [ -e "$path/libplayit2.sh" ]; then
 			PLAYIT_LIB2="$path/libplayit2.sh"
 			break
 		fi
 	done
-	if [ -z "$PLAYIT_LIB2" ]; then
-		printf '\n\033[1;31mError:\033[0m\n'
-		printf 'libplayit2.sh not found.\n'
-		exit 1
-	fi
 fi
+if [ -z "$PLAYIT_LIB2" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'libplayit2.sh not found.\n'
+	exit 1
+fi
+# shellcheck source=play.it-2/lib/libplayit2.sh
 . "$PLAYIT_LIB2"
 
 # Extract game data
 
 extract_data_from "$SOURCE_ARCHIVE"
+tolower "$PLAYIT_WORKDIR/gamedata"
 prepare_package_layout
-
-# Extract icons
-PKG='PKG_DATA' 
-#icons_get_from_workdir 'APP_MAIN' | no icon for this old one
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Write launchers
 
 PKG='PKG_BIN'
-write_launcher 'APP_MAIN'
+launcher_write 'APP_MAIN'
 
 # Build package
 
