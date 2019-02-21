740ddec0e63afe867b6fbd2554c5402b6bb07a54
vv221 <vv221@dotslashplay.it>
2019-02-21 17:42:33 +0100

    Sim City 2000 — Update script to 2.11 syntax + Build a single architecture-independant package

--- play.it-2/games/play-sim-city-2000.sh
+++ play.it-2/games/play-sim-city-2000.sh
@@ -1,9 +1,9 @@
-#!/bin/sh -e
+#!/bin/sh
 set -o errexit
 
 ###
-# Copyright (c) 2015-2018, Antoine Le Gonidec
-# Copyright (c) 2018, Dominique Derrier
+# Copyright (c) 2015-2019, Antoine Le Gonidec
+# Copyright (c) 2018-2019, Dominique Derrier
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -31,11 +31,11 @@ set -o errexit
 
 ###
 # SimCity 2000 | 1993
-# build native Linux packages from the original installers
+# build native packages from the original installers
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20180826.0
+script_version=20190221.2
 
 # Set game-specific variables
 
@@ -49,53 +49,43 @@ ARCHIVE_LTF_MD5='2af6656aac55cd1e2bbada4681c80414'
 ARCHIVE_LTF_SIZE='340000'
 ARCHIVE_LTF_VERSION='1.0-ltf00014'
 
-ARCHIVE_GAME_BIN_PATH='app/c/sc2000'
-ARCHIVE_GAME_BIN_FILES='./*exe ./*cfg ./*mxs ./*bat ./*com ./*txt'
-
-ARCHIVE_GAME_DATA_PATH='app/c/sc2000'
-ARCHIVE_GAME_DATA_FILES='./cities ./scenario ./scurkart ./sound ./*bnk ./*cim ./*urk ./*dat'
-
-#DATA_DIRS='./dunecd'
-# GAME_IMAGE='' |no cd
+ARCHIVE_GAME_MAIN_PATH='app/c/sc2000'
+ARCHIVE_GAME_MAIN_FILES='*.exe *.cfg *.mxs *.bat *.com *.txt *.bnk *.cim *.urk *.dat cities scenario scurkart sound'
 
 APP_MAIN_TYPE='dosbox'
 APP_MAIN_EXE='sc2000.exe'
 APP_MAIN_ICON='app/sc2000.ico'
 
-PACKAGES_LIST='PKG_BIN PKG_DATA'
+PACKAGES_LIST='PKG_MAIN'
 
-PKG_DATA_ID="${GAME_ID}-data"
-PKG_DATA_DESCRIPTION='data'
-
-PKG_BIN_ARCH='32'
-PKG_BIN_DEPS="$PKG_DATA_ID dosbox"
+PKG_MAIN_DEPS='dosbox'
 
 # Load common functions
 
-target_version='2.10'
+target_version='2.11'
 
 if [ -z "$PLAYIT_LIB2" ]; then
-	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
+	: "${XDG_DATA_HOME:="$HOME/.local/share"}"
 	for path in\
-		'./'\
-		"$XDG_DATA_HOME/play.it/"\
-		"$XDG_DATA_HOME/play.it/play.it-2/lib/"\
-		'/usr/local/share/games/play.it/'\
-		'/usr/local/share/play.it/'\
-		'/usr/share/games/play.it/'\
-		'/usr/share/play.it/'
+		"$PWD"\
+		"$XDG_DATA_HOME/play.it"\
+		'/usr/local/share/games/play.it'\
+		'/usr/local/share/play.it'\
+		'/usr/share/games/play.it'\
+		'/usr/share/play.it'
 	do
-		if [ -z "$PLAYIT_LIB2" ] && [ -e "$path/libplayit2.sh" ]; then
+		if [ -e "$path/libplayit2.sh" ]; then
 			PLAYIT_LIB2="$path/libplayit2.sh"
 			break
 		fi
 	done
-	if [ -z "$PLAYIT_LIB2" ]; then
-		printf '\n\033[1;31mError:\033[0m\n'
-		printf 'libplayit2.sh not found.\n'
-		exit 1
-	fi
 fi
+if [ -z "$PLAYIT_LIB2" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'libplayit2.sh not found.\n'
+	exit 1
+fi
+# shellcheck source=play.it-2/lib/libplayit2.sh
 . "$PLAYIT_LIB2"
 
 # Extract game data
@@ -105,14 +95,12 @@ prepare_package_layout
 
 # Extract icons
 
-PKG='PKG_DATA'
 icons_get_from_workdir 'APP_MAIN'
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Write launchers
 
-PKG='PKG_BIN'
-write_launcher 'APP_MAIN'
+launcher_write 'APP_MAIN'
 
 # Build package
 
