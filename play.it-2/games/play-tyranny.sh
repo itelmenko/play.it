ed06fda1ef9e88fcaf0cd8b5cfbbd3200f5b6161
vv221 <vv221@dotslashplay.it>
2019-02-24 18:03:25 +0100

    Tyranny — Update script to 2.11 syntax

--- play.it-2/games/play-tyranny.sh
+++ play.it-2/games/play-tyranny.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/sh
 set -o errexit
 
 ###
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20190115.1
+script_version=20190224.1
 
 # Set game-specific variables
 
@@ -93,7 +93,7 @@ PKG_BIN64_DEPS="$PKG_BIN32_DEPS"
 
 # Load common functions
 
-target_version='2.10'
+target_version='2.11'
 
 if [ -z "$PLAYIT_LIB2" ]; then
 	: "${XDG_DATA_HOME:="$HOME/.local/share"}"
@@ -116,7 +116,7 @@ if [ -z "$PLAYIT_LIB2" ]; then
 	printf 'libplayit2.sh not found.\n'
 	exit 1
 fi
-#shellcheck source=play.it-2/lib/libplayit2.sh
+# shellcheck source=play.it-2/lib/libplayit2.sh
 . "$PLAYIT_LIB2"
 
 # Extract game data
@@ -129,7 +129,7 @@ rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 use_archive_specific_value 'APP_MAIN_EXE_BIN64'
 for PKG in 'PKG_BIN32' 'PKG_BIN64'; do
-	write_launcher 'APP_MAIN'
+	launcher_write 'APP_MAIN'
 done
 
 # Build package
