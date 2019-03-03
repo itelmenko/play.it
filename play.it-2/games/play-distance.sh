9e4647400ff6b2a089f047502398dfa5431c798a
vv221 <vv221@dotslashplay.it>
2019-03-03 19:41:35 +0100

    Distance — Add missing dependency on SDL 2 Fix https://framagit.org/vv221/play.it-games/issues/83

--- play.it-2/games/play-distance.sh
+++ play.it-2/games/play-distance.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20190303.1
+script_version=20190303.2
 
 # Set game-specific variables
 
@@ -71,7 +71,7 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN_ARCH='32'
-PKG_BIN_DEPS="$PKG_DATA_ID glibc libstdc++ glx xcursor libxrandr libudev1"
+PKG_BIN_DEPS="$PKG_DATA_ID glibc libstdc++ glx xcursor libxrandr libudev1 sdl2"
 
 # Load common functions
 
