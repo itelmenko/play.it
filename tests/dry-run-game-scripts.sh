4edfb7929c9dc88b18f8b387ab51ec978bac936a
vv221 <vv221@dotslashplay.it>
2019-03-11 23:39:39 +0100

    New test: Run all game scripts in dry mode once for each supported archive — Make the test fail on script error

--- tests/dry-run-game-scripts.sh
+++ tests/dry-run-game-scripts.sh
@@ -2,6 +2,6 @@
 set -o errexit
 
 tests_directory=$(dirname "$0")
-find 'play.it-2/games' -type f -exec "$tests_directory/dry-run-single-script.sh" '{}' \;
+find 'play.it-2/games' -type f | sort | xargs --verbose -L1 "$tests_directory/dry-run-single-script.sh"
 
 exit 0
