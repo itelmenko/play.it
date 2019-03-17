e031c8e807ecf6df288d44614417155c340b502a
Phil Morrell <debian@emorrp1.name>
2019-03-17 14:25:26 +0000

    Dungeon Keeper 2 - Add glx dep

--- play.it-2/games/play-dungeon-keeper-2.sh
+++ play.it-2/games/play-dungeon-keeper-2.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20190101.1
+script_version=20190317.1
 
 # Set game-specific variables
 
@@ -92,7 +92,7 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN_ARCH='32'
-PKG_BIN_DEPS="$PKG_DATA_ID wine winetricks"
+PKG_BIN_DEPS="$PKG_DATA_ID wine winetricks glx"
 
 # Load common functions
 
