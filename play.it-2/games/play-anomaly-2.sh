997d8203e0a4e9e09e2d014f03c2ff7a687457a1
vv221 <vv221@dotslashplay.it>
2019-04-10 11:36:54 +0200

    Anomaly 2 — Update script to 2.11 syntax

--- play.it-2/games/play-anomaly-2.sh
+++ play.it-2/games/play-anomaly-2.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/sh
 set -o errexit
 
 ###
@@ -30,11 +30,11 @@ set -o errexit
 
 ###
 # Anomaly 2
-# build native Linux packages from the original installers
+# build native packages from the original installers
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20180224.1
+script_version=20190410.1
 
 # Set game-specific variables
 
@@ -49,17 +49,17 @@ ARCHIVE_HUMBLE_MD5='46f0ecd5363106e9eae8642836c29dfc'
 ARCHIVE_HUMBLE_SIZE='2500000'
 ARCHIVE_HUMBLE_VERSION='1.0-humble1'
 
-ARCHIVE_ICONS_PACK='anomaly-2_icons.tar.gz'
-ARCHIVE_ICONS_PACK_MD5='73ddbd1651e08d6c8bb4735e5e0a4a81'
+ARCHIVE_OPTIONAL_ICONS='anomaly-2_icons.tar.gz'
+ARCHIVE_OPTIONAL_ICONS_MD5='73ddbd1651e08d6c8bb4735e5e0a4a81'
 
 ARCHIVE_GAME_BIN_PATH='Anomaly 2 Linux DRM-free'
-ARCHIVE_GAME_BIN_FILES='./Anomaly2 ./libopenal.so.1'
+ARCHIVE_GAME_BIN_FILES='Anomaly2 libopenal.so.1'
 
 ARCHIVE_GAME_DATA_PATH='Anomaly 2 Linux DRM-free'
-ARCHIVE_GAME_DATA_FILES='./*.dat ./*.idx'
+ARCHIVE_GAME_DATA_FILES='*.dat *.idx'
 
 ARCHIVE_ICONS_PATH='.'
-ARCHIVE_ICONS_FILES='./32x32 ./48x48 ./64x64 ./256x256'
+ARCHIVE_ICONS_FILES='32x32 48x48 64x64 256x256'
 
 APP_MAIN_TYPE='native'
 APP_MAIN_PRERUN='gcc -m32 -o preload.so preload.c -ldl -shared -fPIC -Wall -Wextra
@@ -72,32 +72,40 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTIOn='data'
 
 PKG_BIN_ARCH='32'
-PKG_BIN_DEPS_DEB="$PKG_DATA_ID, libgl1-mesa-glx | libgl1, libopenal1, gcc-multilib:amd64 | gcc, libc6-dev-i386:amd64 | libc6-dev"
-PKG_BIN_DEPS_ARCH="$PKG_DATA_ID lib32-libgl lib32-openal gcc-multilib"
+PKG_BIN_DEPS="$PKG_DATA_ID glibc libstdc++ glx openal gcc32"
 
 # Load common functions
 
-target_version='2.5'
+target_version='2.11'
 
 if [ -z "$PLAYIT_LIB2" ]; then
-	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
-	if [ -e "$XDG_DATA_HOME/play.it/libplayit2.sh" ]; then
-		PLAYIT_LIB2="$XDG_DATA_HOME/play.it/libplayit2.sh"
-	elif [ -e './libplayit2.sh' ]; then
-		PLAYIT_LIB2='./libplayit2.sh'
-	else
-		printf '\n\033[1;31mError:\033[0m\n'
-		printf 'libplayit2.sh not found.\n'
-		exit 1
-	fi
+	: "${XDG_DATA_HOME:="$HOME/.local/share"}"
+	for path in\
+		"$PWD"\
+		"$XDG_DATA_HOME/play.it"\
+		'/usr/local/share/games/play.it'\
+		'/usr/local/share/play.it'\
+		'/usr/share/games/play.it'\
+		'/usr/share/play.it'
+	do
+		if [ -e "$path/libplayit2.sh" ]; then
+			PLAYIT_LIB2="$path/libplayit2.sh"
+			break
+		fi
+	done
 fi
-#shellcheck source=play.it-2/lib/libplayit2.sh
+if [ -z "$PLAYIT_LIB2" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'libplayit2.sh not found.\n'
+	exit 1
+fi
+# shellcheck source=play.it-2/lib/libplayit2.sh
 . "$PLAYIT_LIB2"
 
 # Try to load icons archive
 
 ARCHIVE_MAIN="$ARCHIVE"
-set_archive 'ARCHIVE_ICONS' 'ARCHIVE_ICONS_PACK'
+set_archive 'ARCHIVE_ICONS' 'ARCHIVE_OPTIONAL_ICONS'
 ARCHIVE="$ARCHIVE_MAIN"
 
 # Extract game data
@@ -109,24 +117,17 @@ if [ "$ARCHIVE_ICONS" ]; then
 		extract_data_from "$ARCHIVE_ICONS"
 	)
 fi
-
-PKG='PKG_BIN'
-organize_data 'GAME_BIN' "$PATH_GAME"
-
-PKG='PKG_DATA'
-organize_data 'GAME_DATA' "$PATH_GAME"
-
-PKG='PKG_DATA'
+prepare_package_layout
 if [ "$ARCHIVE_ICONS" ]; then
+	PKG='PKG_DATA'
 	organize_data 'ICONS' "$PATH_ICON_BASE"
 fi
-
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Write launchers
 
 PKG='PKG_BIN'
-write_launcher 'APP_MAIN'
+launchers_write 'APP_MAIN'
 
 # Hack to work around infinite loading times
 
