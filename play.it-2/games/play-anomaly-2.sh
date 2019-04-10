f3df6bee99ea413bcce0aed4207f343cf2b8e8c5
vv221 <vv221@dotslashplay.it>
2019-04-10 18:33:20 +0200

    Anomaly 2 — Add support for Windows version Fix https://framagit.org/vv221/play.it-games/issues/99

--- play.it-2/games/play-anomaly-2.sh
+++ play.it-2/games/play-anomaly-2.sh
@@ -34,29 +34,41 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20190410.4
+script_version=20190410.6
 
 # Set game-specific variables
 
 GAME_ID='anomaly-2'
 GAME_NAME='Anomaly 2'
 
-ARCHIVES_LIST='ARCHIVE_HUMBLE'
+ARCHIVES_LIST='ARCHIVE_LINUX_HUMBLE ARCHIVE_WINDOWS_HUMBLE'
 
-ARCHIVE_HUMBLE='Anomaly2_Linux_1387299615.zip'
-ARCHIVE_HUMBLE_URL='https://www.humblebundle.com/store/anomaly-2'
-ARCHIVE_HUMBLE_MD5='46f0ecd5363106e9eae8642836c29dfc'
-ARCHIVE_HUMBLE_SIZE='2500000'
-ARCHIVE_HUMBLE_VERSION='1.0-humble1'
+ARCHIVE_LINUX_HUMBLE='Anomaly2_Linux_1387299615.zip'
+ARCHIVE_LINUX_HUMBLE_URL='https://www.humblebundle.com/store/anomaly-2'
+ARCHIVE_LINUX_HUMBLE_MD5='46f0ecd5363106e9eae8642836c29dfc'
+ARCHIVE_LINUX_HUMBLE_SIZE='2500000'
+ARCHIVE_LINUX_HUMBLE_VERSION='1.0-humble1'
+
+ARCHIVE_WINDOWS_HUMBLE='Anomaly2_Windows_1387299615.zip'
+ARCHIVE_WINDOWS_HUMBLE_URL='https://www.humblebundle.com/store/anomaly-2'
+ARCHIVE_WINDOWS_HUMBLE_MD5='2b5ccffcbaee8cfebfd4bb74cacb9fbc'
+ARCHIVE_WINDOWS_HUMBLE_SIZE='2500000'
+ARCHIVE_WINDOWS_HUMBLE_VERSION='1.0-humble1'
 
 ARCHIVE_OPTIONAL_ICONS='anomaly-2_icons.tar.gz'
 ARCHIVE_OPTIONAL_ICONS_MD5='73ddbd1651e08d6c8bb4735e5e0a4a81'
 
-ARCHIVE_GAME_BIN_PATH='Anomaly 2 Linux DRM-free'
-ARCHIVE_GAME_BIN_FILES='Anomaly2 libopenal.so.1'
+ARCHIVE_GAME_BIN_PATH_LINUX='Anomaly 2 Linux DRM-free'
+ARCHIVE_GAME_BIN_PATH_WINDOWS='Anomaly 2 Windows DRM-free/2013-12-17 03-20'
+ARCHIVE_GAME_BIN_FILES='*.exe *.dll *.txt Anomaly2 libopenal.so.1'
 
-ARCHIVE_GAME_DATA_PATH='Anomaly 2 Linux DRM-free'
-ARCHIVE_GAME_DATA_FILES='*.dat *.idx'
+ARCHIVE_GAME_COMMON_PATH_LINUX='Anomaly 2 Linux DRM-free'
+ARCHIVE_GAME_COMMON_PATH_WINDOWS='Anomaly 2 Windows DRM-free/2013-12-17 03-20'
+ARCHIVE_GAME_COMMON_FILES='videos.dat voices.dat'
+
+ARCHIVE_GAME_DATA_PATH_LINUX='Anomaly 2 Linux DRM-free'
+ARCHIVE_GAME_DATA_PATH_WINDOWS='Anomaly 2 Windows DRM-free/2013-12-17 03-20'
+ARCHIVE_GAME_DATA_FILES='*.idx *.str animations.dat common.dat scenes.dat sounds.dat templates.dat textures.dat'
 
 ARCHIVE_ICONS_PATH='.'
 ARCHIVE_ICONS_FILES='32x32 48x48 64x64 256x256'
@@ -65,8 +77,9 @@ CONFIG_FILES='./userdata/config.bin'
 DATA_DIRS='./userdata/DefaultUser'
 DATA_FILES='./userdata/iPhoneProfiles'
 
-APP_MAIN_TYPE='native'
-APP_MAIN_PRERUN='NEW_LAUNCH_REQUIRED=0
+APP_MAIN_TYPE_LINUX='native'
+# shellcheck disable=SC2016
+APP_MAIN_PRERUN_LINUX='NEW_LAUNCH_REQUIRED=0
 if [ -e "$HOME/.Anomaly 2" ] && [ ! -h "$HOME/.Anomaly 2" ]; then
 	NEW_LAUNCH_REQUIRED=1
 	if [ -e "$HOME/.Anomaly 2/config.bin" ]; then
@@ -95,18 +108,35 @@ gcc -m32 -o preload.so preload.c -ldl -shared -fPIC -Wall -Wextra
 pulseaudio --start
 LD_PRELOAD=./preload.so
 export LD_PRELOAD'
-APP_MAIN_EXE='Anomaly2'
+APP_MAIN_EXE_LINUX='Anomaly2'
 
-PACKAGES_LIST='PKG_DATA PKG_BIN'
+APP_MAIN_TYPE_WINDOWS='wine'
+APP_MAIN_EXE_WINDOWS='Anomaly 2.exe'
+APP_MAIN_ICON_WINDOWS='Anomaly 2.exe'
+
+PACKAGES_LIST='PKG_COMMON PKG_DATA PKG_BIN'
+
+PKG_COMMON_ID="${GAME_ID}-data-common"
+PKG_COMMON_DESCRIPTIOn='data — common'
 
 PKG_DATA_ID="${GAME_ID}-data"
-PKG_DATA_DESCRIPTIOn='data'
+PKG_DATA_ID_LINUX="${PKG_DATA_ID}-linux"
+PKG_DATA_ID_WINDOWS="${PKG_DATA_ID}-windows"
+PKG_DATA_PROVIDE="$PKG_DATA_ID"
+PKG_DATA_DESCRIPTION='data'
+PKG_DATA_DESCRIPTION_LINUX="$PKG_DATA_DESCRIPTION — Linux"
+PKG_DATA_DESCRIPTION_WINDOWS="$PKG_DATA_DESCRIPTION — Windows"
 
+PKG_BIN_ID="$GAME_ID"
+PKG_BIN_ID_LINUX="${PKG_BIN_ID}-linux"
+PKG_BIN_ID_WINDOWS="${PKG_BIN_ID}-windows"
+PKG_BIN_PROVIDE="$PKG_BIN_ID"
 PKG_BIN_ARCH='32'
-PKG_BIN_DEPS="$PKG_DATA_ID glibc libstdc++ glx openal gcc32 pulseaudio"
-PKG_BIN_DEPS_ARCH='lib32-libpulse lib32-libx11'
-PKG_BIN_DEPS_DEB='libpulse0, libx11-6'
-PKG_BIN_DEPS_GENTOO='media-sound/pulseaudio[abi_x86_32] x11-libs/libX11[abi_x86_32]'
+PKG_BIN_DEPS_LINUX="$PKG_COMMON_ID $PKG_DATA_ID glibc libstdc++ glx openal gcc32 pulseaudio"
+PKG_BIN_DEPS_ARCH_LINUX='lib32-libpulse lib32-libx11'
+PKG_BIN_DEPS_DEB_LINUX='libpulse0, libx11-6'
+PKG_BIN_DEPS_GENTOO_LINUX='media-sound/pulseaudio[abi_x86_32] x11-libs/libX11[abi_x86_32]'
+PKG_BIN_DEPS_WINDOWS="$PKG_COMMON_ID $PKG_DATA_ID wine glx xcursor"
 
 # Load common functions
 
@@ -138,54 +168,97 @@ fi
 
 # Try to load icons archive
 
-ARCHIVE_MAIN="$ARCHIVE"
-set_archive 'ARCHIVE_ICONS' 'ARCHIVE_OPTIONAL_ICONS'
-ARCHIVE="$ARCHIVE_MAIN"
+case "$ARCHIVE" in
+	('ARCHIVE_LINUX'*)
+		ARCHIVE_MAIN="$ARCHIVE"
+		set_archive 'ARCHIVE_ICONS' 'ARCHIVE_OPTIONAL_ICONS'
+		ARCHIVE="$ARCHIVE_MAIN"
+	;;
+esac
 
 # Extract game data
 
 extract_data_from "$SOURCE_ARCHIVE"
-if [ "$ARCHIVE_ICONS" ]; then
-	(
-		ARCHIVE='ARCHIVE_ICONS'
-		extract_data_from "$ARCHIVE_ICONS"
-	)
-fi
 prepare_package_layout
-if [ "$ARCHIVE_ICONS" ]; then
-	PKG='PKG_DATA'
-	organize_data 'ICONS' "$PATH_ICON_BASE"
-fi
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
+
+# Get icons
+
+case "$ARCHIVE" in
+	('ARCHIVE_LINUX'*)
+		if [ "$ARCHIVE_ICONS" ]; then
+			(
+				# shellcheck disable=SC2030
+				ARCHIVE='ARCHIVE_ICONS'
+				extract_data_from "$ARCHIVE_ICONS"
+			)
+			PKG='PKG_DATA'
+			organize_data 'ICONS' "$PATH_ICON_BASE"
+		fi
+	;;
+	('ARCHIVE_WINDOWS'*)
+		PKG='PKG_BIN'
+		use_archive_specific_value 'APP_MAIN_ICON'
+		icons_get_from_package 'APP_MAIN'
+		icons_move_to 'PKG_DATA'
+	;;
+esac
 
 # Write launchers
 
 PKG='PKG_BIN'
+use_archive_specific_value 'APP_MAIN_TYPE'
+use_archive_specific_value 'APP_MAIN_PRERUN'
+use_archive_specific_value 'APP_MAIN_EXE'
 launchers_write 'APP_MAIN'
 
 # Hack to work around infinite loading times
 
-cat > "${PKG_BIN_PATH}${PATH_GAME}/preload.c" << EOF
-#define _GNU_SOURCE
-#include <dlfcn.h>
-#include <semaphore.h>
-#include <stdio.h>
-#include <time.h>
-#include <unistd.h>
+# shellcheck disable=SC2031
+case "$ARCHIVE" in
+	('ARCHIVE_LINUX'*)
+		if [ $DRY_RUN -eq 0 ]; then
+			cat > "${PKG_BIN_PATH}${PATH_GAME}/preload.c" <<- EOF
+			#define _GNU_SOURCE
+			#include <dlfcn.h>
+			#include <semaphore.h>
+			#include <stdio.h>
+			#include <time.h>
+			#include <unistd.h>
 
-static int (*_realSemTimedWait)(sem_t *, const struct timespec *) = NULL;
+			static int (*_realSemTimedWait)(sem_t *, const struct timespec *) = NULL;
 
-int sem_timedwait(sem_t *sem, const struct timespec *abs_timeout) {
-	if (abs_timeout->tv_nsec >= 1000000000) {
-		((struct timespec *)abs_timeout)->tv_nsec -= 1000000000;
-		((struct timespec *)abs_timeout)->tv_sec++;
-	}
-	return _realSemTimedWait(sem, abs_timeout);
-}
-__attribute__((constructor)) void init(void) {
-	_realSemTimedWait = dlsym(RTLD_NEXT, "sem_timedwait");
-}
-EOF
+			int sem_timedwait(sem_t *sem, const struct timespec *abs_timeout) {
+			    if (abs_timeout->tv_nsec >= 1000000000) {
+			        ((struct timespec *)abs_timeout)->tv_nsec -= 1000000000;
+			        ((struct timespec *)abs_timeout)->tv_sec++;
+			    }
+			    return _realSemTimedWait(sem, abs_timeout);
+			}
+			__attribute__((constructor)) void init(void) {
+			    _realSemTimedWait = dlsym(RTLD_NEXT, "sem_timedwait");
+			}
+			EOF
+		fi
+	;;
+esac
+
+# Store saved games and settings outside of WINE prefix
+
+# shellcheck disable=SC2031
+case "$ARCHIVE" in
+	('ARCHIVE_WINDOWS'*)
+		# shellcheck disable=SC2016
+		saves_path='$WINEPREFIX/drive_c/users/$(whoami)/Application Data/11bitstudios/Anomaly 2'
+		# shellcheck disable=SC2016
+		pattern='s#init_prefix_dirs "$PATH_DATA" "$DATA_DIRS"#&'
+		pattern="$pattern\\nif [ ! -e \"$saves_path\" ]; then"
+		pattern="$pattern\\n\\tmkdir --parents \"${saves_path%/*}\""
+		pattern="$pattern\\n\\tln --symbolic \"\$PATH_DATA/userdata\" \"$saves_path\""
+		pattern="$pattern\\nfi#"
+		sed --in-place "$pattern" "${PKG_BIN_PATH}${PATH_BIN}"/*
+	;;
+esac
 
 # Build package
 
