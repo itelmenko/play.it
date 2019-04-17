fedef82941f92d88b46a7ef8ba3b1179cf27f2a3
Mopi <mopi@dotslashplay.it>
2019-04-17 15:47:04 +0200

    New game: Dream Daddy

---
+++ play.it-2/games/play-dream-daddy.sh
@@ -0,0 +1,147 @@
+#!/bin/sh -e
+set -o errexit
+
+###
+# Copyright (c) 2015-2018, Antoine Le Gonidec
+# Copyright (c) 2016-2018, Solène Huault
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# This software is provided by the copyright holders and contributors "as is"
+# and any express or implied warranties, including, but not limited to, the
+# implied warranties of merchantability and fitness for a particular purpose
+# are disclaimed. In no event shall the copyright holder or contributors be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of the
+# possibility of such damage.
+###
+
+###
+# Dream Daddy
+# build native Linux packages from the original installers
+# send your bug reports to mopi@dotslashplay.it
+###
+
+script_version=20190417.1
+
+# Set game-specific variables
+
+GAME_ID='dream-daddy'
+GAME_NAME='Dream Daddy'
+
+ARCHIVES_LIST='ARCHIVE_HUMBLE'
+
+ARCHIVE_HUMBLE='dreamdaddy_adaddatingsimulator_linux.zip'
+ARCHIVE_HUMBLE_URL='https://www.humblebundle.com/store/dream-daddy-a-dad-dating-simulator?'
+ARCHIVE_HUMBLE_MD5='a191cb84040c25919aa1bd029180cf11'
+ARCHIVE_HUMBLE_SIZE='500000'
+ARCHIVE_HUMBLE_VERSION='0.11-humble1'
+
+ARCHIVE_DOC_PATH='data'
+ARCHIVE_DOC_FILES='Linux.README'
+
+ARCHIVE_GAME_BIN32_PATH='data'
+ARCHIVE_GAME_BIN32_FILES='*.x86 *_Data/*/x86'
+
+ARCHIVE_GAME_BIN64_PATH='data'
+ARCHIVE_GAME_BIN64_FILES='*.x86_64 *_Data/*/x86_64'
+
+ARCHIVE_GAME_DATA_PATH='data'
+ARCHIVE_GAME_DATA_FILES='*_Data'
+
+APP_MAIN_TYPE='native'
+APP_MAIN_EXE_BIN32='DDaDDs.x86'
+APP_MAIN_EXE_BIN64='DDaDDs.x86_64'
+APP_MAIN_ICON='DDaDDs_Data/Resources/UnityPlayer.png'
+
+PACKAGES_LIST='PKG_BIN32 PKG_BIN64 PKG_DATA'
+
+PKG_DATA_ID="${GAME_ID}-data"
+PKG_DATA_DESCRIPTION='data'
+
+PKG_BIN32_ARCH='32'
+PKG_BIN32_DEPS="$PKG_DATA_ID glibc libstdc++"
+
+PKG_BIN64_ARCH='64'
+PKG_BIN64_DEPS="$PKG_BIN32_DEPS"
+
+# Load common functions
+
+target_version='2.11'
+
+if [ -z "$PLAYIT_LIB2" ]; then
+	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
+	for path in\
+		'./'\
+		"$XDG_DATA_HOME/play.it/"\
+		"$XDG_DATA_HOME/play.it/play.it-2/lib/"\
+		'/usr/local/share/games/play.it/'\
+		'/usr/local/share/play.it/'\
+		'/usr/share/games/play.it/'\
+		'/usr/share/play.it/'
+	do
+		if [ -z "$PLAYIT_LIB2" ] && [ -e "$path/libplayit2.sh" ]; then
+			PLAYIT_LIB2="$path/libplayit2.sh"
+			break
+		fi
+	done
+	if [ -z "$PLAYIT_LIB2" ]; then
+		printf '\n\033[1;31mError:\033[0m\n'
+		printf 'libplayit2.sh not found.\n'
+		exit 1
+	fi
+fi
+#shellcheck source=play.it-2/lib/libplayit2.sh
+. "$PLAYIT_LIB2"
+
+# Extract game data
+
+extract_data_from "$SOURCE_ARCHIVE"
+
+ARCHIVE_HUMBLE_TYPE='mojosetup'
+extract_data_from "$PLAYIT_WORKDIR/gamedata/Dream Daddy Linux/dreamdaddy-02142019-bin"
+rm "$PLAYIT_WORKDIR/gamedata/Dream Daddy Linux/dreamdaddy-02142019-bin"
+rm --recursive --force "$PLAYIT_WORKDIR/gamedata/guis" "$PLAYIT_WORKDIR/gamedata/meta" "$PLAYIT_WORKDIR/gamedata/scripts"
+
+set_standard_permissions "$PLAYIT_WORKDIR/gamedata"
+
+prepare_package_layout
+
+rm --recursive "$PLAYIT_WORKDIR/gamedata"
+
+# Write launchers
+
+for PKG in 'PKG_BIN32' 'PKG_BIN64'; do
+	launcher_write 'APP_MAIN'
+done
+
+# Build package
+
+PKG='PKG_DATA'
+icons_linking_postinst 'APP_MAIN'
+write_metadata 'PKG_DATA'
+write_metadata 'PKG_BIN32' 'PKG_BIN64'
+build_pkg
+
+# Clean up
+
+rm --recursive "$PLAYIT_WORKDIR"
+
+# Print instructions
+
+print_instructions
+
+exit 0
