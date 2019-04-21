7d3911102b627f8aa9f397b0c6188b8c05a9b332
BetaRays <BetaRays+framagit@protonmail.com>
2019-04-21 16:36:39 +0200

    Another Lost Phone: Laura’s Story — Fix copyright

--- play.it-2/games/play-another-lost-phone-lauras-story.sh
+++ play.it-2/games/play-another-lost-phone-lauras-story.sh
@@ -2,8 +2,8 @@
 set -o errexit
 
 ###
-# Copyright (c) 2015-2018, Antoine Le Gonidec
-# Copyright (c) 2018, Solène Huault
+# Copyright (c) 2015-2019, Antoine Le Gonidec
+# Copyright (c) 2018-2019, BetaRays
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
