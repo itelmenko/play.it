d651db243f2279d28f4e1f6164a8a3b48aeb8680
vv221 <vv221@dotslashplay.it>
2019-04-28 21:51:30 +0200

    System Shock 2 — New archives

--- play.it-2/games/play-system-shock-2.sh
+++ play.it-2/games/play-system-shock-2.sh
@@ -34,17 +34,30 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20190428.1
+script_version=20190520.1
 
 # Set game-specific variables
 
 GAME_ID='system-shock-2'
 GAME_NAME='System Shock 2'
 
-ARCHIVE_GOG='setup_system_shock_2_2.46_update_2_(18733).exe'
-ARCHIVE_GOG_MD5='39fab64451ace95966988bb90c7bb17e'
-ARCHIVE_GOG_SIZE='680000'
-ARCHIVE_GOG_VERSION='2.46.2-gog18733'
+ARCHIVE_GOG='setup_system_shock_2_2.47_nd_(22087).exe'
+ARCHIVE_GOG_URL='https://www.gog.com/game/system_shock_2'
+ARCHIVE_GOG_MD5='cc2ff390b566364447dc5bd05757fe57'
+ARCHIVE_GOG_SIZE='670000'
+ARCHIVE_GOG_VERSION='2.47-gog22087'
+ARCHIVE_GOG_TYPE='innosetup1.7'
+
+ARCHIVE_GOG_OLD3='setup_system_shock_2_2.46_update_3_(19935).exe'
+ARCHIVE_GOG_OLD3_MD5='cdafcdea01556eccab899f94503843df'
+ARCHIVE_GOG_OLD3_SIZE='670000'
+ARCHIVE_GOG_OLD3_VERSION='2.46.3-gog19935'
+ARCHIVE_GOG_OLD3_TYPE='innosetup1.7'
+
+ARCHIVE_GOG_OLD2='setup_system_shock_2_2.46_update_2_(18733).exe'
+ARCHIVE_GOG_OLD2_MD5='39fab64451ace95966988bb90c7bb17e'
+ARCHIVE_GOG_OLD2_SIZE='680000'
+ARCHIVE_GOG_OLD2_VERSION='2.46.2-gog18733'
 
 ARCHIVE_GOG_OLD1='setup_system_shock_2_2.46_update_(18248).exe'
 ARCHIVE_GOG_OLD1_MD5='b76803e4a632b58527eada8993999143'
@@ -56,17 +69,33 @@ ARCHIVE_GOG_OLD0_MD5='98c3d01d53bb2b0dc25d7ed7093a67d3'
 ARCHIVE_GOG_OLD0_SIZE='680000'
 ARCHIVE_GOG_OLD0_VERSION='2.46-gog11004'
 
-ARCHIVE_DOC_DATA_PATH='app'
+ARCHIVE_DOC_DATA_PATH='.'
 ARCHIVE_DOC_DATA_FILES='*.pdf *.txt *.wri doc editor/*.txt'
+# Keep compatibility with old archives
+ARCHIVE_DOC_DATA_PATH_GOG_OLD1='app'
+ARCHIVE_DOC_DATA_PATH_GOG_OLD2='app'
+ARCHIVE_DOC_DATA_PATH_GOG_OLD0='app'
 
-ARCHIVE_GAME0_BIN_PATH='app'
+ARCHIVE_GAME0_BIN_PATH='.'
 ARCHIVE_GAME0_BIN_FILES='*.ax *.bnd *.cfg *.exe *.osm 7z.dll d3dx9_43.dll ffmpeg.dll fmsel.dll ir41_32.dll ir50_32.dll lgvid.dll msvcrt40.dll editor/*.cfg editor/*.dll editor/*.exe microsoft.vc90.crt'
+# Keep compatibility with old archives
+ARCHIVE_GAME0_BIN_PATH_GOG_OLD1='app'
+ARCHIVE_GAME0_BIN_PATH_GOG_OLD2='app'
+ARCHIVE_GAME0_BIN_PATH_GOG_OLD0='app'
 
-ARCHIVE_GAME1_BIN_PATH='app/__support/app'
+ARCHIVE_GAME1_BIN_PATH='__support/app'
 ARCHIVE_GAME1_BIN_FILES='*.cfg *.ini'
+# Keep compatibility with old archives
+ARCHIVE_GAME1_BIN_PATH_GOG_OLD1='app/__support/app'
+ARCHIVE_GAME1_BIN_PATH_GOG_OLD2='app/__support/app'
+ARCHIVE_GAME1_BIN_PATH_GOG_OLD0='app/__support/app'
 
-ARCHIVE_GAME_DATA_PATH='app'
+ARCHIVE_GAME_DATA_PATH='.'
 ARCHIVE_GAME_DATA_FILES='*.bin *.dif *.dml ilist.* patch* binds data sq_scripts'
+# Keep compatibility with old archives
+ARCHIVE_GAME_DATA_PATH_GOG_OLD1='app'
+ARCHIVE_GAME_DATA_PATH_GOG_OLD2='app'
+ARCHIVE_GAME_DATA_PATH_GOG_OLD0='app'
 
 CONFIG_FILES='./*.bnd ./*.cfg ./*.ini'
 DATA_DIRS='./current ./save_0 ./save_1 ./save_2 ./save_3 ./save_4 ./save_5 ./save_6 ./save_7 ./save_8 ./save_9 ./save_10 ./save_11 ./save_12 ./save_13 ./save_14'
