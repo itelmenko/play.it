3abe53f8e31d568a23a9035a0b160fceacd9baed
vv221 <vv221@dotslashplay.it>
2019-05-04 20:15:39 +0200

    Planescape Torment - Enhanced Edition — Syntax update
    * Update script to 2.11 syntax
    * Update authors list

--- play.it-2/games/play-planescape-torment-enhanced-edition.sh
+++ play.it-2/games/play-planescape-torment-enhanced-edition.sh
@@ -1,8 +1,9 @@
-#!/bin/sh -e
+#!/bin/sh
 set -o errexit
 
 ###
 # Copyright (c) 2015-2019, Antoine "vv221/vv222" Le Gonidec
+# Copyright (c) 2016-2019, Solène "Mopi" Huault
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -30,18 +31,16 @@ set -o errexit
 
 ###
 # Planescape Torment - Enhanced Edition
-# build native Linux packages from the original installers
+# build native packages from the original installers
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20180224.1
+script_version=20190504.1
 
 # Set game-specific variables
 
 GAME_ID='planescape-torment-enhanced-edition'
 GAME_NAME='Planescape Torment - Enhanced Edition'
-
-ARCHIVES_LIST='ARCHIVE_GOG'
 
 ARCHIVE_GOG='gog_planescape_torment_enhanced_edition_2.1.0.3.sh'
 ARCHIVE_GOG_URL='https://www.gog.com/game/planescape_torment_enhanced_edition'
@@ -50,29 +49,28 @@ ARCHIVE_GOG_SIZE='1800000'
 ARCHIVE_GOG_VERSION='3.1.3-gog2.1.0.3'
 ARCHIVE_GOG_TYPE='mojosetup'
 
-ARCHIVE_LIBSSL_32='libssl_1.0.0_32-bit.tar.gz'
-ARCHIVE_LIBSSL_32_MD5='9443cad4a640b2512920495eaf7582c4'
+ARCHIVE_OPTIONAL_LIBSSL32='libssl_1.0.0_32-bit.tar.gz'
+ARCHIVE_OPTIONAL_LIBSSL32_MD5='9443cad4a640b2512920495eaf7582c4'
 
-ARCHIVE_LIBSSL_64='libssl_1.0.0_64-bit.tar.gz'
-ARCHIVE_LIBSSL_64_MD5='89917bef5dd34a2865cb63c2287e0bd4'
+ARCHIVE_OPTIONAL_LIBSSL64='libssl_1.0.0_64-bit.tar.gz'
+ARCHIVE_OPTIONAL_LIBSSL64_MD5='89917bef5dd34a2865cb63c2287e0bd4'
 
 ARCHIVE_DOC_DATA_PATH='data/noarch/docs'
-ARCHIVE_DOC_DATA_FILES='./*'
+ARCHIVE_DOC_DATA_FILES='*'
 
 ARCHIVE_GAME_BIN32_PATH='data/noarch/game'
-ARCHIVE_GAME_BIN32_FILES='./Torment ./*_Data/*/x86'
+ARCHIVE_GAME_BIN32_FILES='Torment'
 
 ARCHIVE_GAME_BIN64_PATH='data/noarch/game'
-ARCHIVE_GAME_BIN64_FILES='./Torment64 ./*_Data/*/x86_64'
+ARCHIVE_GAME_BIN64_FILES='Torment64'
 
 ARCHIVE_GAME_DATA_PATH='data/noarch/game'
-ARCHIVE_GAME_DATA_FILES='./Torment.pdb ./music ./lang ./engine.lua ./data ./chitin.key'
+ARCHIVE_GAME_DATA_FILES='Torment.pdb music lang engine.lua data chitin.key'
 
 APP_MAIN_TYPE='native'
 APP_MAIN_EXE_BIN32='Torment'
 APP_MAIN_EXE_BIN64='Torment64'
 APP_MAIN_ICON='data/noarch/support/icon.png'
-APP_MAIN_ICON_RES='256'
 
 PACKAGES_LIST='PKG_DATA PKG_BIN32 PKG_BIN64'
 
@@ -89,83 +87,85 @@ PKG_BIN64_DEPS_ARCH='openssl-1.0'
 
 # Load common functions
 
-target_version='2.5'
+target_version='2.11'
 
 if [ -z "$PLAYIT_LIB2" ]; then
-	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
-	if [ -e "$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh" ]; then
-		PLAYIT_LIB2="$XDG_DATA_HOME/play.it/play.it-2/lib/libplayit2.sh"
-	elif [ -e './libplayit2.sh' ]; then
-		PLAYIT_LIB2='./libplayit2.sh'
-	else
-		printf '\n\033[1;31mError:\033[0m\n'
-		printf 'libplayit2.sh not found.\n'
-		exit 1
-	fi
+	: "${XDG_DATA_HOME:="$HOME/.local/share"}"
+	for path in\
+		"$PWD"\
+		"$XDG_DATA_HOME/play.it"\
+		'/usr/local/share/games/play.it'\
+		'/usr/local/share/play.it'\
+		'/usr/share/games/play.it'\
+		'/usr/share/play.it'
+	do
+		if [ -e "$path/libplayit2.sh" ]; then
+			PLAYIT_LIB2="$path/libplayit2.sh"
+			break
+		fi
+	done
 fi
-#shellcheck source=play.it-2/lib/libplayit2.sh
+if [ -z "$PLAYIT_LIB2" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'libplayit2.sh not found.\n'
+	exit 1
+fi
+# shellcheck source=play.it-2/lib/libplayit2.sh
 . "$PLAYIT_LIB2"
 
 # Use libSSL 1.0.0 archives
 
-if [ "$OPTION_PACKAGE" != 'arch' ]; then
-	ARCHIVE_MAIN="$ARCHIVE"
-	set_archive 'ARCHIVE_LIBSSL32' 'ARCHIVE_LIBSSL_32'
-	set_archive 'ARCHIVE_LIBSSL64' 'ARCHIVE_LIBSSL_64'
-	ARCHIVE="$ARCHIVE_MAIN"
-fi
+case "$OPTION_PACKAGE" in
+	('arch') ;;
+	(*)
+		ARCHIVE_MAIN="$ARCHIVE"
+		set_archive 'ARCHIVE_LIBSSL32' 'ARCHIVE_OPTIONAL_LIBSSL32'
+		set_archive 'ARCHIVE_LIBSSL64' 'ARCHIVE_OPTIONAL_LIBSSL64'
+		ARCHIVE="$ARCHIVE_MAIN"
+	;;
+esac
 
 # Extract game data
 
 extract_data_from "$SOURCE_ARCHIVE"
+prepare_package_layout
 
-for PKG in $PACKAGES_LIST; do
-	organize_data "DOC_${PKG#PKG_}"  "$PATH_DOC"
-	organize_data "GAME_${PKG#PKG_}" "$PATH_GAME"
-done
+# Extract icons
 
 PKG='PKG_DATA'
-get_icon_from_temp_dir 'APP_MAIN'
-
+icons_get_from_workdir 'APP_MAIN'
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Include libSSL into the game directory
 
-# shellcheck disable=SC2153
-if [ "$ARCHIVE_LIBSSL32" ]; then
+if [ -n "$ARCHIVE_LIBSSL32" ]; then
 	(
 		ARCHIVE='ARCHIVE_LIBSSL32'
 		extract_data_from "$ARCHIVE_LIBSSL32"
 	)
-	dir='libs'
-	mkdir --parents "${PKG_BIN32_PATH}${PATH_GAME}/$dir"
-	mv "$PLAYIT_WORKDIR/gamedata"/* "${PKG_BIN32_PATH}${PATH_GAME}/$dir"
-	APP_MAIN_LIBS_BIN32="$dir"
+	mkdir --parents "${PKG_BIN32_PATH}${PATH_GAME}/${APP_MAIN_LIBS_BIN32:=libs}"
+	mv "$PLAYIT_WORKDIR/gamedata"/* "${PKG_BIN32_PATH}${PATH_GAME}/$APP_MAIN_LIBS_BIN32"
 	rm --recursive "$PLAYIT_WORKDIR/gamedata"
 fi
-# shellcheck disable=SC2153
-if [ "$ARCHIVE_LIBSSL64" ]; then
+if [ -n "$ARCHIVE_LIBSSL64" ]; then
 	(
 		ARCHIVE='ARCHIVE_LIBSSL64'
 		extract_data_from "$ARCHIVE_LIBSSL64"
 	)
-	dir='libs'
-	mkdir --parents "${PKG_BIN64_PATH}${PATH_GAME}/$dir"
-	mv "$PLAYIT_WORKDIR/gamedata"/* "${PKG_BIN64_PATH}${PATH_GAME}/$dir"
-	APP_MAIN_LIBS_BIN64="$dir"
+	mkdir --parents "${PKG_BIN64_PATH}${PATH_GAME}/${APP_MAIN_LIBS_BIN64:=libs}"
+	mv "$PLAYIT_WORKDIR/gamedata"/* "${PKG_BIN64_PATH}${PATH_GAME}/$APP_MAIN_LIBS_BIN64"
 	rm --recursive "$PLAYIT_WORKDIR/gamedata"
 fi
 
 # Write launchers
 
 for PKG in 'PKG_BIN32' 'PKG_BIN64'; do
-	write_launcher 'APP_MAIN'
+	launchers_write 'APP_MAIN'
 done
 
 # Build package
 
-write_metadata 'PKG_DATA'
-write_metadata 'PKG_BIN32' 'PKG_BIN64'
+write_metadata
 build_pkg
 
 # Clean up
@@ -174,10 +174,6 @@ rm --recursive "$PLAYIT_WORKDIR"
 
 # Print instructions
 
-printf '\n'
-printf '32-bit:'
-print_instructions 'PKG_DATA' 'PKG_BIN32'
-printf '64-bit:'
-print_instructions 'PKG_DATA' 'PKG_BIN64'
+print_instructions
 
 exit 0
