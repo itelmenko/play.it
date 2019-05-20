6a92b6787c1806db16be3829a328dd5f4578dda1
vv221 <vv221@dotslashplay.it>
2019-05-20 23:11:47 +0200

    Owlboy — Fix dependencies list

--- play.it-2/games/play-owlboy.sh
+++ play.it-2/games/play-owlboy.sh
@@ -35,7 +35,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20190505.1
+script_version=20190626.2
 
 # Set game-specific variables
 
@@ -82,10 +82,16 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN32_ARCH='32'
-PKG_BIN32_DEPS="$PKG_DATA_ID glibc libstdc++"
+PKG_BIN32_DEPS="$PKG_DATA_ID glibc libstdc++ sdl2 vorbis glx libudev1"
+PKG_BIN32_DEPS_ARCH='lib32-zlib'
+PKG_BIN32_DEPS_DEB='zlib1g'
+PKG_BIN32_DEPS_GENTOO='sys-libs/zlib[abi_x86_32]'
 
 PKG_BIN64_ARCH='64'
 PKG_BIN64_DEPS="$PKG_BIN32_DEPS"
+PKG_BIN64_DEPS_ARCH='zlib'
+PKG_BIN64_DEPS_DEB="$PKG_BIN32_DEPS_DEB"
+PKG_BIN64_DEPS_GENTOO='sys-libs/zlib'
 
 # Load common functions
 
