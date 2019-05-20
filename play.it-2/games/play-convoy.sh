c7e3639d1ab4fb804b678618a2728d078d12936e
vv221 <vv221@dotslashplay.it>
2019-05-20 19:23:13 +0200

    Convoy — Fix dependencies list

--- play.it-2/games/play-convoy.sh
+++ play.it-2/games/play-convoy.sh
@@ -35,7 +35,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20190520.12
+script_version=20190520.13
 
 # Set game-specific variables
 
@@ -76,10 +76,16 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN32_ARCH='32'
-PKG_BIN32_DEPS="$PKG_DATA_ID glibc libstdc++ glu xcursor"
+PKG_BIN32_DEPS="$PKG_DATA_ID glibc libstdc++ glu glx xcursor"
+PKG_BIN32_DEPS_ARCH='lib32-libx11'
+PKG_BIN32_DEPS_DEB='libx11-6'
+PKG_BIN32_DEPS_GENTOO='x11-libs/libX11[abi_x86_32]'
 
 PKG_BIN64_ARCH='64'
 PKG_BIN64_DEPS="$PKG_BIN32_DEPS"
+PKG_BIN64_DEPS_ARCH='libx11'
+PKG_BIN64_DEPS_DEB="$PKG_BIN32_DEPS_DEB"
+PKG_BIN64_DEPS_GENTOO='x11-libs/libX11'
 
 # Load common functions
 
