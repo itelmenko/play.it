5e941c2cd0138b8ec1815ed4363b61bf2104bffd
BetaRays <BetaRays+playit@protonmail.com>
2019-08-10 13:30:14 +0200

    Deprecate icons_linking_postinst

--- play.it-2/src/50_compatibility.sh
+++ play.it-2/src/50_compatibility.sh
@@ -1,3 +1,57 @@
+# Keep compatibility with 2.11.2 and older
+
+icons_linking_postinst() {
+	[ "$DRY_RUN" = '1' ] && return 0
+	case "$OPTION_PACKAGE" in
+		('arch')
+			local app
+			local file
+			local icon
+			local list
+			local name
+			local path
+			local path_icon
+			local path_pkg
+			local version_major_target
+			local version_minor_target
+			# shellcheck disable=SC2154
+			version_major_target="${target_version%%.*}"
+			# shellcheck disable=SC2154
+			version_minor_target=$(printf '%s' "$target_version" | cut --delimiter='.' --fields=2)
+			path_pkg="$(get_value "${PKG}_PATH")"
+			[ -n "$path_pkg" ] || missing_pkg_error 'icons_linking_postinst' "$PKG"
+			path="${path_pkg}${PATH_GAME}"
+			for app in "$@"; do
+				list="$(get_value "${app}_ICONS_LIST")"
+				[ "$list" ] || list="${app}_ICON"
+				name="$(get_value "${app}_ID")"
+				[ "$name" ] || name="$GAME_ID"
+				for icon in $list; do
+					file="$(get_value "$icon")"
+					if [ $version_major_target -lt 2 ] || [ $version_minor_target -lt 8 ]; then
+						# ensure compatibility with scripts targeting pre-2.8 library
+						if [ -e "$path/$file" ] || [ -e "$path"/$file ]; then
+							icon_get_resolution_from_file "$path/$file"
+						else
+							icon_get_resolution_from_file "${PKG_DATA_PATH}${PATH_GAME}/$file"
+						fi
+					else
+						icon_get_resolution_from_file "$path/$file"
+					fi
+					path_icon="$PATH_ICON_BASE/$resolution/apps"
+					cat >> "$prerm" <<- EOF
+					if [ -e "$path_icon/$name.png" ]; then
+					rm "$path_icon/$name.png"
+					rmdir --parents --ignore-fail-on-non-empty "$path_icon"
+					fi
+					EOF
+				done
+			done
+		;;
+	esac
+	icons_get_from_package "$@"
+}
+
 # Keep compatibility with 2.10 and older
 
 write_bin() {
