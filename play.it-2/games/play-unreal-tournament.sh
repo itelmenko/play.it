6acfe1619648d315e3041a636253a6520f4ecccb
vv221 <vv221@dotslashplay.it>
2019-09-15 18:01:34 +0200

    Unreal Tournament — Add missing dependency on libGL.so.1

--- play.it-2/games/play-unreal-tournament.sh
+++ play.it-2/games/play-unreal-tournament.sh
@@ -35,7 +35,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20190721.1
+script_version=20190915.1
 
 # Set game-specific variables
 
@@ -85,7 +85,7 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN_ARCH='32'
-PKG_BIN_DEPS="$PKG_DATA_ID glibc libstdc++ sdl1.2 pulseaudio"
+PKG_BIN_DEPS="$PKG_DATA_ID glibc libstdc++ sdl1.2 pulseaudio glx"
 PKG_BIN_DEPS_ARCH='lib32-libpulse'
 PKG_BIN_DEPS_DEB='libpulsedsp'
 
