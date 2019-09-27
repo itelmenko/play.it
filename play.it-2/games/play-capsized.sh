977ca59ae4e2153f1692ad448df3b29606c16e20
vv221 <vv221@dotslashplay.it>
2019-09-27 18:32:00 +0200

    Capsized — Syntax update

--- play.it-2/games/play-capsized.sh
+++ play.it-2/games/play-capsized.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/sh
 set -o errexit
 
 ###
@@ -31,11 +31,11 @@ set -o errexit
 
 ###
 # Capsized
-# build native Linux packages from the original installers
+# build native packages from the original installers
 # send your bug reports to dev+playit@indigo.re
 ###
 
-script_version=20190717.1
+script_version=20190927.1
 
 # Set game-specific variables
 
@@ -45,12 +45,12 @@ GAME_NAME='Capsized'
 ARCHIVE_GOG='gog_capsized_2.0.0.2.sh'
 ARCHIVE_GOG_URL='https://www.gog.com/game/capsized'
 ARCHIVE_GOG_MD5='f516c52b4614d63c3cfa4e1ed43934b8'
-ARCHIVE_GOG_SIZE='668045312'
-ARCHIVE_GOG_VERSION='2.0.0.2-gog06132013'
+ARCHIVE_GOG_SIZE='670000'
+ARCHIVE_GOG_VERSION='1.0-gog2.0.0.2'
 ARCHIVE_GOG_TYPE='mojosetup'
 
 ARCHIVE_DOC_DATA_PATH='data/noarch/docs'
-ARCHIVE_DOC_DATA_FILES='./*'
+ARCHIVE_DOC_DATA_FILES='*'
 
 ARCHIVE_GAME_BIN32_PATH='data/noarch/game'
 ARCHIVE_GAME_BIN32_FILES='NePlusUltra.bin.x86 lib'
@@ -79,31 +79,30 @@ PKG_BIN64_DEPS="$PKG_BIN32_DEPS"
 
 # Load common functions
 
-target_version='2.10'
+target_version='2.11'
 
 if [ -z "$PLAYIT_LIB2" ]; then
-        [ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
-        for path in\
-                './'\
-                "$XDG_DATA_HOME/play.it/"\
-                "$XDG_DATA_HOME/play.it/play.it-2/lib/"\
-                '/usr/local/share/games/play.it/'\
-                '/usr/local/share/play.it/'\
-                '/usr/share/games/play.it/'\
-                '/usr/share/play.it/'
-        do
-                if [ -z "$PLAYIT_LIB2" ] && [ -e "$path/libplayit2.sh" ]; then
-                        PLAYIT_LIB2="$path/libplayit2.sh"
-                        break
-                fi
-        done
-        if [ -z "$PLAYIT_LIB2" ]; then
-                printf '\n\033[1;31mError:\033[0m\n'
-                printf 'libplayit2.sh not found.\n'
-                exit 1
-        fi
+	: "${XDG_DATA_HOME:="$HOME/.local/share"}"
+	for path in\
+		"$PWD"\
+		"$XDG_DATA_HOME/play.it"\
+		'/usr/local/share/games/play.it'\
+		'/usr/local/share/play.it'\
+		'/usr/share/games/play.it'\
+		'/usr/share/play.it'
+	do
+		if [ -e "$path/libplayit2.sh" ]; then
+			PLAYIT_LIB2="$path/libplayit2.sh"
+			break
+		fi
+	done
 fi
-#shellcheck source=play.it-2/lib/libplayit2.sh
+if [ -z "$PLAYIT_LIB2" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'libplayit2.sh not found.\n'
+	exit 1
+fi
+# shellcheck source=play.it-2/lib/libplayit2.sh
 . "$PLAYIT_LIB2"
 
 # Extract game data
@@ -120,7 +119,7 @@ rm --recursive "$PLAYIT_WORKDIR/gamedata"
 # Write launchers
 
 for PKG in PKG_BIN32 PKG_BIN64; do
-        write_launcher 'APP_MAIN'
+        launchers_write 'APP_MAIN'
 done
 
 # Build package
@@ -130,7 +129,7 @@ build_pkg
 
 # Clean up
 
-rm --recursive "${PLAYIT_WORKDIR}"
+rm --recursive "$PLAYIT_WORKDIR"
 
 # Print instructions
 
