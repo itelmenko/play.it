19e4724a548e5e95d6b9c6a1a6f5178b45d7120e
vv221 <vv221@dotslashplay.it>
2019-09-27 19:49:17 +0200

    Tetrobot and Co. — Syntax update
    * Use game-provided icon instead of GOG one

--- play.it-2/games/play-tetrobot-and-co.sh
+++ play.it-2/games/play-tetrobot-and-co.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/sh
 set -o errexit
 
 ###
@@ -31,11 +31,11 @@ set -o errexit
 
 ###
 # Tetrobot and Co.
-# build native Linux packages from the original installers
+# build native packages from the original installers
 # send your bug reports to dev+playit@indigo.re
 ###
 
-script_version=20190717.1
+script_version=20190927.1
 
 # Set game-specific variables
 
@@ -45,76 +45,73 @@ GAME_NAME='Tetrobot and Co.'
 ARCHIVE_GOG='gog_tetrobot_and_co_2.1.0.6.sh'
 ARCHIVE_GOG_URL='https://www.gog.com/game/tetrobot-and-co'
 ARCHIVE_GOG_MD5='2ad2969e64e19d5753f8822e407c148c'
-ARCHIVE_GOG_SIZE='536285184'
-ARCHIVE_GOG_VERSION='2.1.0.6-gog1.2.1'
-ARCHIVE_GOG_TYPE='mojosetup'
+ARCHIVE_GOG_SIZE='530000'
+ARCHIVE_GOG_VERSION='1.2.1-gog2.1.0.6'
 
 ARCHIVE_DOC_DATA_PATH='data/noarch/docs'
-ARCHIVE_DOC_DATA_FILES='./*'
+ARCHIVE_DOC_DATA_FILES='*'
 
-ARCHIVE_GAME_BIN32_PATH='data/noarch/game'
-ARCHIVE_GAME_BIN32_FILES='Data/Mono/x86 Tetrobot?and?Co.x86'
+ARCHIVE_GAME_BIN_PATH='data/noarch/game'
+ARCHIVE_GAME_BIN_FILES='Tetrobot?and?Co.x86 Data/Mono'
 
 ARCHIVE_GAME_DATA_PATH='data/noarch/game'
-ARCHIVE_GAME_DATA_FILES='Data/*.assets Data/Managed Data/Mono/etc Data/Resources Data/level0 Data/level1 Data/level2 Data/level3 Data/level4 Data/level5 Data/level6 Data/mainData'
+ARCHIVE_GAME_DATA_FILES='Data'
 
 APP_MAIN_TYPE='native'
-APP_MAIN_EXE_BIN32='Tetrobot and Co.x86'
-APP_MAIN_ICON='data/noarch/support/icon.png'
+APP_MAIN_EXE='Tetrobot and Co.x86'
+APP_MAIN_ICON='Data/Resources/UnityPlayer.png'
 
-PACKAGES_LIST='PKG_DATA PKG_BIN32'
+PACKAGES_LIST='PKG_BIN PKG_DATA'
 
 PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
-PKG_BIN32_ARCH='32'
-PKG_BIN32_DEPS="$PKG_DATA_ID glu glx libstdc++ xcursor"
+PKG_BIN_ARCH='32'
+PKG_BIN_DEPS="$PKG_DATA_ID glu glx libstdc++ xcursor"
 
 # Load common functions
 
-target_version='2.10'
+target_version='2.11'
 
 if [ -z "$PLAYIT_LIB2" ]; then
-        [ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
-        for path in\
-                './'\
-                "$XDG_DATA_HOME/play.it/"\
-                "$XDG_DATA_HOME/play.it/play.it-2/lib/"\
-                '/usr/local/share/games/play.it/'\
-                '/usr/local/share/play.it/'\
-                '/usr/share/games/play.it/'\
-                '/usr/share/play.it/'
-        do
-                if [ -z "$PLAYIT_LIB2" ] && [ -e "$path/libplayit2.sh" ]; then
-                        PLAYIT_LIB2="$path/libplayit2.sh"
-                        break
-                fi
-        done
-        if [ -z "$PLAYIT_LIB2" ]; then
-                printf '\n\033[1;31mError:\033[0m\n'
-                printf 'libplayit2.sh not found.\n'
-                exit 1
-        fi
+	: "${XDG_DATA_HOME:="$HOME/.local/share"}"
+	for path in\
+		"$PWD"\
+		"$XDG_DATA_HOME/play.it"\
+		'/usr/local/share/games/play.it'\
+		'/usr/local/share/play.it'\
+		'/usr/share/games/play.it'\
+		'/usr/share/play.it'
+	do
+		if [ -e "$path/libplayit2.sh" ]; then
+			PLAYIT_LIB2="$path/libplayit2.sh"
+			break
+		fi
+	done
 fi
-#shellcheck source=play.it-2/lib/libplayit2.sh
+if [ -z "$PLAYIT_LIB2" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'libplayit2.sh not found.\n'
+	exit 1
+fi
+# shellcheck source=play.it-2/lib/libplayit2.sh
 . "$PLAYIT_LIB2"
 
 # Extract game data
 
 extract_data_from "$SOURCE_ARCHIVE"
 prepare_package_layout
+rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Extract icon
 
 PKG='PKG_DATA'
-icons_get_from_workdir 'APP_MAIN'
-rm --recursive "$PLAYIT_WORKDIR/gamedata"
+icons_get_from_package 'APP_MAIN'
 
 # Write launchers
 
-for PKG in PKG_BIN32; do
-        write_launcher 'APP_MAIN'
-done
+PKG='PKG_BIN'
+launchers_write 'APP_MAIN'
 
 # Build package
 
@@ -123,7 +120,7 @@ build_pkg
 
 # Clean up
 
-rm --recursive "${PLAYIT_WORKDIR}"
+rm --recursive "$PLAYIT_WORKDIR"
 
 # Print instructions
 
